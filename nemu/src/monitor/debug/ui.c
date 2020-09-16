929841bfb677275feda70cca51aa5f8028f390bb
tracer-ics2020 <tracer@njuics.org>
2020-09-16 14:34:35 +0800

    >  compile 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: 60f2595d059e4784a9b57adf745ff12d     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  14:34:35 up  1:53,  1 user,  load average: 0.00, 0.01, 0.01

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -55,7 +55,8 @@ static int cmd_info(char *args ) {
 
 static int cmd_x(char *args ) {
   unsigned int num=0,address_start=0;
-  char *arg1 = strtok(NULL,""),*arg2 = strtok(NULL,"");
+  char *arg1 = strtok(NULL,"");
+  char *arg2 = strtok(NULL,"");
   sscanf(arg1,"%d",&num);
   sscanf(arg2,"0x%x",&address_start);
   printf("0x%x \n",address_start);
