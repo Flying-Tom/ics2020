2396ddbdcdbc9611dac2c5616dd3c697d5cc4dfa
tracer-ics2020 <tracer@njuics.org>
2020-09-16 16:54:00 +0800

    >  compile 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: 60f2595d059e4784a9b57adf745ff12d     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  16:54:01 up  4:12,  1 user,  load average: 0.11, 0.04, 0.01

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -78,6 +78,7 @@ static bool make_token(char *e) {
 
         Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
             i, rules[i].regex, position, substr_len, substr_len, substr_start);
+        printf("YES\n");
 
         position += substr_len;
 
