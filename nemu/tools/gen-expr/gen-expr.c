a6e249d90edb06638a95eb910b5fd98173e34a04
tracer-ics2020 <tracer@njuics.org>
2020-09-18 19:18:51 +0800

    >  gdb 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: ff261e542bd94209b44d676531e7349f     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  19:18:51 up  1:06,  1 user,  load average: 0.29, 0.17, 0.07

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -16,7 +16,7 @@ uint32_t choose(uint32_t n){
 }
 
 static inline void gen_num(){
-  uint32_t num_size = choose(2) + 1;
+  uint32_t num_size = choose(1) + 1;
   buf[bufptr] = choose(10) + 48;
   if(buf[bufptr++] == '0' )
   return;
@@ -88,7 +88,7 @@ int main(int argc, char *argv[]) {
     bufptr = 0;
     memset(buf,'\0',sizeof(buf));
     gen_rand_expr();
-    if(bufptr > 32)
+    if(bufptr > 50)
     continue;
 
     sprintf(code_buf, code_format, buf);
