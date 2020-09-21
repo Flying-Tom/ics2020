20087e280df9e1a1ba0f6f7671967c5cb202379e
tracer-ics2020 <tracer@njuics.org>
2020-09-21 09:45:59 +0800

    >  compile 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: ecc68f5fac9b4ab7afdac9cd34b3e480     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  09:45:59 up  1:20,  1 user,  load average: 0.08, 0.12, 0.09

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -46,7 +46,7 @@ void insert_wp(WP *wp,char* args){
   }
   
   wp->NO = cnt + 1;
-  *wp->expr = *args;
+  sscanf(args,"%s",wp->expr);
   wp->value=expr(args, &success);
   wp->next = NULL;
   printf("watchpoint %d: %s\n",wp->NO,args);
