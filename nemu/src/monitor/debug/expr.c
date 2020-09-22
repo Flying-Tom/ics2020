f3a864a5175612c03967b965f1181263929906c0
tracer-ics2020 <tracer@njuics.org>
2020-09-22 23:46:44 +0800

    >  compile 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: 432029f7f5f64209bae11868a87a73df     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  23:46:45 up  2:27,  1 user,  load average: 0.14, 0.08, 0.02

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -345,7 +345,7 @@ word_t expr(char *e, bool *success) {
   if ( tokens[0].type == '-' && (tokens[1].type == TK_HEXNUM  ||  tokens[1].type == TK_DECNUM || tokens[1].type == TK_REG) ) 
     tokens[0].type = TK_NEG;
 
-  for (i=0;i< nr_token - 1;i++) 
+  for (i = 1; i < nr_token ;i++) 
   {
     if (tokens[i].type == '*' && (tokens[i+1].type == TK_HEXNUM  ||  tokens[i+1].type == TK_DECNUM || tokens[i+1].type == TK_REG) ) 
       tokens[i].type = TK_DEREF;
