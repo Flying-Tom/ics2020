9ff8187559170b22895252cb768b11ebc0bb6ebb
tracer-ics2020 <tracer@njuics.org>
2020-09-28 08:09:09 +0800

    >  compile 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: 9678d1de2f654f16bed0cf444612a38c     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  08:09:10 up 5 min,  1 user,  load average: 0.62, 0.66, 0.34

--- nemu/src/isa/x86/reg.c
+++ nemu/src/isa/x86/reg.c
@@ -7,51 +7,66 @@ const char *regsl[] = {"eax", "ecx", "edx", "ebx", "esp", "ebp", "esi", "edi"};
 const char *regsw[] = {"ax", "cx", "dx", "bx", "sp", "bp", "si", "di"};
 const char *regsb[] = {"al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"};
 
-void reg_test() {
-  srand(time(0));
-  word_t sample[8];
-  word_t pc_sample = rand();
-  cpu.pc = pc_sample;
+void reg_test()
+{
+    srand(time(0));
+    word_t sample[8];
+    word_t pc_sample = rand();
+    cpu.pc = pc_sample;
 
-  int i;
-  for (i = R_EAX; i <= R_EDI; i ++) {
-    sample[i] = rand();
-    reg_l(i) = sample[i];
-    assert(reg_w(i) == (sample[i] & 0xffff));
-  }
+    int i;
+    for (i = R_EAX; i <= R_EDI; i++)
+    {
+        sample[i] = rand();
+        reg_l(i) = sample[i];
+        assert(reg_w(i) == (sample[i] & 0xffff));
+    }
 
-  assert(reg_b(R_AL) == (sample[R_EAX] & 0xff));
-  assert(reg_b(R_AH) == ((sample[R_EAX] >> 8) & 0xff));
-  assert(reg_b(R_BL) == (sample[R_EBX] & 0xff));
-  assert(reg_b(R_BH) == ((sample[R_EBX] >> 8) & 0xff));
-  assert(reg_b(R_CL) == (sample[R_ECX] & 0xff));
-  assert(reg_b(R_CH) == ((sample[R_ECX] >> 8) & 0xff));
-  assert(reg_b(R_DL) == (sample[R_EDX] & 0xff));
-  assert(reg_b(R_DH) == ((sample[R_EDX] >> 8) & 0xff));
+    assert(reg_b(R_AL) == (sample[R_EAX] & 0xff));
+    assert(reg_b(R_AH) == ((sample[R_EAX] >> 8) & 0xff));
+    assert(reg_b(R_BL) == (sample[R_EBX] & 0xff));
+    assert(reg_b(R_BH) == ((sample[R_EBX] >> 8) & 0xff));
+    assert(reg_b(R_CL) == (sample[R_ECX] & 0xff));
+    assert(reg_b(R_CH) == ((sample[R_ECX] >> 8) & 0xff));
+    assert(reg_b(R_DL) == (sample[R_EDX] & 0xff));
+    assert(reg_b(R_DH) == ((sample[R_EDX] >> 8) & 0xff));
 
-  assert(sample[R_EAX] == cpu.eax);
-  assert(sample[R_ECX] == cpu.ecx);
-  assert(sample[R_EDX] == cpu.edx);
-  assert(sample[R_EBX] == cpu.ebx);
-  assert(sample[R_ESP] == cpu.esp);
-  assert(sample[R_EBP] == cpu.ebp);
-  assert(sample[R_ESI] == cpu.esi);
-  assert(sample[R_EDI] == cpu.edi);
+    assert(sample[R_EAX] == cpu.eax);
+    assert(sample[R_ECX] == cpu.ecx);
+    assert(sample[R_EDX] == cpu.edx);
+    assert(sample[R_EBX] == cpu.ebx);
+    assert(sample[R_ESP] == cpu.esp);
+    assert(sample[R_EBP] == cpu.ebp);
+    assert(sample[R_ESI] == cpu.esi);
+    assert(sample[R_EDI] == cpu.edi);
 
-  assert(pc_sample == cpu.pc);
+    assert(pc_sample == cpu.pc);
 }
 
-void isa_reg_display() {
-    printf("$eax = %d\n",cpu.eax);
-    printf("$ecx = %d\n",cpu.ecx);
-    printf("$edx = %d\n",cpu.edx);
-    printf("$ebx = %d\n",cpu.ebx);
-    printf("$esp = %d\n",cpu.esp);
-    printf("$ebp = %d\n",cpu.ebp);
-    printf("$esi = %d\n",cpu.esi);
-    printf("$edi = %d\n",cpu.edi);  
+void isa_reg_display()
+{
+    printf("$eax = %d\n", cpu.eax);
+    printf("$ecx = %d\n", cpu.ecx);
+    printf("$edx = %d\n", cpu.edx);
+    printf("$ebx = %d\n", cpu.ebx);
+    printf("$esp = %d\n", cpu.esp);
+    printf("$ebp = %d\n", cpu.ebp);
+    printf("$esi = %d\n", cpu.esi);
+    printf("$edi = %d\n", cpu.edi);
 }
 
-word_t isa_reg_str2val(const char *s, bool *success) {
-  return 0;
+word_t isa_reg_str2val(const char *s, bool *success)
+{
+    *success = true;
+    int i;
+    for (i = 0; i <= 7; i++)
+    {
+        if (strcmp(regsl[i], s) == 0)
+            return cpu.gpr[i]._32;
+        if (strcmp(regsw[i], s) == 0)
+            return cpu.gpr[i]._16;
+        if (strcmp(regsb[i], s) == 0)
+            return cpu.gpr[i]._8[1];
+    }
+    return 0;
 }
