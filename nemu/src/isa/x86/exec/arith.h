c74d2c7648bbe6fe989d57b29661fd263a0574de
tracer-ics2020 <tracer@njuics.org>
2020-10-09 23:22:02 +0800

    >  compile 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: b748fc245ecc45258e288cca091e8c08     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  23:22:02 up  2:09,  1 user,  load average: 0.12, 0.06, 0.01

--- nemu/src/isa/x86/exec/arith.h
+++ nemu/src/isa/x86/exec/arith.h
@@ -22,8 +22,9 @@ static inline def_EHelper(sub)
 {
     if (id_src1->width == 1 && id_dest->width >= 2)
         rtl_sext(s, dsrc1, dsrc1, id_src1->width);
-    rtl_sub(s, s0, ddest, dsrc1);
-    operand_write(s, id_dest, s0);
+    //rtl_sub(s, s0, ddest, dsrc1);
+    rtl_sub(s, ddest, ddest, dsrc1);
+    //operand_write(s, id_dest, s0);
     print_asm_template2(sub);
 }
 
