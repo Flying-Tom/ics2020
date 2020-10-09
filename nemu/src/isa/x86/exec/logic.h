547649eee43b6df19e03cfdb60f45142ed687e1b
tracer-ics2020 <tracer@njuics.org>
2020-10-09 13:21:16 +0800

    >  compile 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: d1af797b86844014be19af769ea2ed07     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  13:21:17 up 58 min,  1 user,  load average: 0.42, 0.32, 0.22

--- nemu/src/isa/x86/exec/logic.h
+++ nemu/src/isa/x86/exec/logic.h
@@ -1,61 +1,70 @@
 #include "cc.h"
 
-
-static inline def_EHelper(test) {
-  TODO();
-  print_asm_template2(test);
+static inline def_EHelper(test)
+{
+    TODO();
+    print_asm_template2(test);
 }
 
-static inline def_EHelper(and) {
-  TODO();
-  print_asm_template2(and);
+static inline def_EHelper(and)
+{
+    TODO();
+    print_asm_template2(and);
 }
 
-static inline def_EHelper(xor) {
-  TODO();
-
-  print_asm_template2(xor);
+static inline def_EHelper(xor)
+{
+    rtl_xor(s, s0, ddest, dsrc1);
+    operand_write(s, id_dest, s0);
+    rtl_sm(s, s0, 0, 0, 4);
+    rtl_set_CF(s,s0);
+    rtl_set_OF(s,s0);
+    print_asm_template2(xor);
 }
 
-static inline def_EHelper(or) {
-  TODO();
+static inline def_EHelper(or)
+{
+    TODO();
 
-  print_asm_template2(or);
+    print_asm_template2(or);
 }
 
-static inline def_EHelper(sar) {
-  TODO();
-  // unnecessary to update CF and OF in NEMU
+static inline def_EHelper(sar)
+{
+    TODO();
+    // unnecessary to update CF and OF in NEMU
 
-  print_asm_template2(sar);
+    print_asm_template2(sar);
 }
 
-static inline def_EHelper(shl) {
-  TODO();
-  // unnecessary to update CF and OF in NEMU
+static inline def_EHelper(shl)
+{
+    TODO();
+    // unnecessary to update CF and OF in NEMU
 
-  print_asm_template2(shl);
+    print_asm_template2(shl);
 }
 
-static inline def_EHelper(shr) {
-  TODO();
-  // unnecessary to update CF and OF in NEMU
+static inline def_EHelper(shr)
+{
+    TODO();
+    // unnecessary to update CF and OF in NEMU
 
-  print_asm_template2(shr);
+    print_asm_template2(shr);
 }
 
+static inline def_EHelper(setcc)
+{
+    uint32_t cc = s->opcode & 0xf;
+    rtl_setcc(s, ddest, cc);
+    operand_write(s, id_dest, ddest);
 
-static inline def_EHelper(setcc) {
-  uint32_t cc = s->opcode & 0xf;
-  rtl_setcc(s, ddest, cc);
-  operand_write(s, id_dest, ddest);
-
-  print_asm("set%s %s", get_cc_name(cc), id_dest->str);
+    print_asm("set%s %s", get_cc_name(cc), id_dest->str);
 }
 
-static inline def_EHelper(not) {
-  TODO();
+static inline def_EHelper(not )
+{
+    TODO();
 
-  print_asm_template1(not);
+    print_asm_template1(not );
 }
-
