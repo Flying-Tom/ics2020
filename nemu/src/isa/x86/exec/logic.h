cda369855aa3c5ade0906bb2708707c0b48fb15d
tracer-ics2020 <tracer@njuics.org>
2020-10-09 13:22:46 +0800

    >  compile 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: d1af797b86844014be19af769ea2ed07     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  13:22:46 up 59 min,  1 user,  load average: 0.27, 0.31, 0.22

--- nemu/src/isa/x86/exec/logic.h
+++ nemu/src/isa/x86/exec/logic.h
@@ -16,7 +16,7 @@ static inline def_EHelper(xor)
 {
     rtl_xor(s, s0, ddest, dsrc1);
     operand_write(s, id_dest, s0);
-    rtl_sm(s, s0, 0, 0, 1);
+    rtl_li(s, s0, 0);
     rtl_set_CF(s,s0);
     rtl_set_OF(s,s0);
     print_asm_template2(xor);
