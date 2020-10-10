b36b1300633fa6aab5bf6235e2f95b6d28918077
tracer-ics2020 <tracer@njuics.org>
2020-10-10 19:42:15 +0800

    >  compile 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: 6504b1b8d5fc47be84eeede87023fad8     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  19:42:15 up  2:47,  1 user,  load average: 1.10, 1.05, 1.01

--- nemu/include/rtl/pseudo.h
+++ nemu/include/rtl/pseudo.h
@@ -17,12 +17,12 @@ static inline def_rtl(mv, rtlreg_t* dest, const rtlreg_t *src1) {
 
 static inline def_rtl(not, rtlreg_t *dest, const rtlreg_t* src1) {
   // dest <- ~src1
-  TODO();
+  *dest = ~ *src1;
 }
 
 static inline def_rtl(neg, rtlreg_t *dest, const rtlreg_t* src1) {
   // dest <- -src1
-  // rtl_(s,dest,src1,width * 8 - 1);
+   *dest = -*src1;
 }
 
 static inline def_rtl(sext, rtlreg_t* dest, const rtlreg_t* src1, int width) {
--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -23,7 +23,7 @@ static inline def_EHelper(gp1)
         EMPTY(2)
         EMPTY(3)
         EMPTY(4) //////////////
-        EXW(5,sub,4)//EXW(5,sub,1)
+        EX(5,sub)//EXW(5,sub,1)?
         EMPTY(6) //////////////
         EMPTY(7)
     }
