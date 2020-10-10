82f0c7f6e53d4048316de03d6ee26fe2aeabbfcd
tracer-ics2020 <tracer@njuics.org>
2020-10-10 13:51:13 +0800

    >  compile 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: 6504b1b8d5fc47be84eeede87023fad8     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  13:51:13 up  1:07,  1 user,  load average: 1.24, 1.07, 0.80

--- nemu/include/rtl/pseudo.h
+++ nemu/include/rtl/pseudo.h
@@ -22,7 +22,7 @@ static inline def_rtl(not, rtlreg_t *dest, const rtlreg_t* src1) {
 
 static inline def_rtl(neg, rtlreg_t *dest, const rtlreg_t* src1) {
   // dest <- -src1
-  TODO();
+  // rtl_(s,dest,src1,width * 8 - 1);
 }
 
 static inline def_rtl(sext, rtlreg_t* dest, const rtlreg_t* src1, int width) {
