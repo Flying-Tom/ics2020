3803a6c80dbb634ca878fa42b44bd58aaee09029
tracer-ics2020 <tracer@njuics.org>
2020-10-14 00:19:45 +0800

    >  compile 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: 735b3050b71f416bab5bc0e92db9bdf7     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  00:19:45 up  1:44,  1 user,  load average: 0.13, 0.10, 0.09

--- nemu/src/isa/x86/exec/arith.h
+++ nemu/src/isa/x86/exec/arith.h
@@ -15,7 +15,7 @@ static inline def_EHelper(add)
 // dest <- sub result
 static inline void cmp_internal(DecodeExecState *s)
 {
-    rtl_sub(s, s0, dsrc1, dsrc2);
+    rtl_sub(s, s0, ddest, dsrc1);
     rtl_update_ZFSF(s, s0, id_dest->width);
     rtl_is_sub_carry(s, s1, ddest, dsrc1);
     rtl_set_CF(s, s1);
