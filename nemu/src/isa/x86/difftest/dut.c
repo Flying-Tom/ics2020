ba2c90a6fa0273181abb8f6a3fd3ba50889a0844
tracer-ics2020 <tracer@njuics.org>
2020-10-13 18:55:59 +0800

    >  compile 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: f47169fb3fc14d0fae442a19d8e80f2c     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  18:56:00 up  3:33,  1 user,  load average: 0.60, 0.51, 0.32

--- nemu/src/isa/x86/difftest/dut.c
+++ nemu/src/isa/x86/difftest/dut.c
@@ -13,7 +13,17 @@ static void checkregs(CPU_state *ref_r, char *s, bool *judg)
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc)
 {
     if(ref_r->eax !=cpu.eax || ref_r->ecx !=cpu.ecx || ref_r->edx !=cpu.edx || ref_r->ebx !=cpu.ebx || ref_r->esp !=cpu.esp || ref_r->ebp !=cpu.ebp || ref_r->esi !=cpu.esi || ref_r->edi !=cpu.edi)
+    {
+        printf("eax: NEMU:%d QEMU:%d\n",cpu.eax,ref_r->eax);
+        printf("ecx: NEMU:%d QEMU:%d\n",cpu.ecx,ref_r->ecx);
+        printf("edx: NEMU:%d QEMU:%d\n",cpu.edx,ref_r->edx);
+        printf("ebx: NEMU:%d QEMU:%d\n",cpu.ebx,ref_r->ebx);
+        printf("esp: NEMU:%d QEMU:%d\n",cpu.esp,ref_r->esp);
+        printf("ebp: NEMU:%d QEMU:%d\n",cpu.ebp,ref_r->ebp);
+        printf("esi: NEMU:%d QEMU:%d\n",cpu.esi,ref_r->esi);
+        printf("edi: NEMU:%d QEMU:%d\n",cpu.edi,ref_r->edi);
         return false;
+    }
     else
         return true;
 }
