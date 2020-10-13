22758cbdb6547a8b22cd993c399f2135711302d3
tracer-ics2020 <tracer@njuics.org>
2020-10-14 01:01:54 +0800

    >  compile 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: 735b3050b71f416bab5bc0e92db9bdf7     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  01:01:54 up  2:26,  1 user,  load average: 0.12, 0.10, 0.04

--- nemu/include/rtl/pseudo.h
+++ nemu/include/rtl/pseudo.h
@@ -7,36 +7,66 @@
 
 /* RTL pseudo instructions */
 
-static inline def_rtl(li, rtlreg_t* dest, const rtlreg_t imm) {
-  rtl_addi(s, dest, rz, imm);
+static inline def_rtl(li, rtlreg_t *dest, const rtlreg_t imm)
+{
+    rtl_addi(s, dest, rz, imm);
 }
 
-static inline def_rtl(mv, rtlreg_t* dest, const rtlreg_t *src1) {
-  if (dest != src1) rtl_add(s, dest, src1, rz);
+static inline def_rtl(mv, rtlreg_t *dest, const rtlreg_t *src1)
+{
+    if (dest != src1)
+        rtl_add(s, dest, src1, rz);
 }
 
-static inline def_rtl(not, rtlreg_t *dest, const rtlreg_t* src1) {
-  // dest <- ~src1
-  *dest = ~ *src1;
+static inline def_rtl(not, rtlreg_t *dest, const rtlreg_t *src1)
+{
+    // dest <- ~src1
+    *dest = ~*src1;
 }
 
-static inline def_rtl(neg, rtlreg_t *dest, const rtlreg_t* src1) {
-  // dest <- -src1
-   *dest = -*src1;
+static inline def_rtl(neg, rtlreg_t *dest, const rtlreg_t *src1)
+{
+    // dest <- -src1
+    *dest = -*src1;
 }
 
-static inline def_rtl(sext, rtlreg_t* dest, const rtlreg_t* src1, int width) {
-  // dest <- signext(src1[(width * 8 - 1) .. 0])
-  TODO();
+static inline def_rtl(sext, rtlreg_t *dest, const rtlreg_t *src1, int width)
+{
+    // dest <- signext(src1[(width * 8 - 1) .. 0])
+    switch (width)
+    {
+    case 1:
+        *dest = (int8_t)(*src1);
+        break;
+    case 2:
+        *dest = (int16_t)(*src1);
+        break;
+    case 4:
+        *dest = (int32_t)(*src1);
+        break;
+    }
 }
 
-static inline def_rtl(zext, rtlreg_t* dest, const rtlreg_t* src1, int width) {
-  // dest <- zeroext(src1[(width * 8 - 1) .. 0])
-  TODO();
+static inline def_rtl(zext, rtlreg_t *dest, const rtlreg_t *src1, int width)
+{
+    // dest <- zeroext(src1[(width * 8 - 1) .. 0])
+    switch (width)
+    {
+    case 1:
+        *dest = (uint8_t)(*src1);
+        break;
+    case 2:
+        *dest = (uint16_t)(*src1);
+        break;
+    case 4:
+        *dest = (uint32_t)(*src1);
+        break;
+    }
 }
 
-static inline def_rtl(msb, rtlreg_t* dest, const rtlreg_t* src1, int width) {
-  // dest <- src1[width * 8 - 1]
-  rtl_shri(s,dest,src1,width * 8 - 1);
+static inline def_rtl(msb, rtlreg_t *dest, const rtlreg_t *src1, int width)
+{
+    // dest <- src1[width * 8 - 1]
+    rtl_shri(s, dest, src1, width * 8 - 1);
 }
 #endif
