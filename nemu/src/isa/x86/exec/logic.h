06e7261184f392cff695e077dce31428e9869d5e
tracer-ics2020 <tracer@njuics.org>
2020-10-14 14:47:06 +0800

    >  compile 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: 5627b174aa5f4b1fb3dbf7d7ca1f2f15     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  14:47:06 up  1:46,  1 user,  load average: 0.25, 0.15, 0.11

--- nemu/src/isa/x86/exec/logic.h
+++ nemu/src/isa/x86/exec/logic.h
@@ -4,15 +4,16 @@ static inline def_EHelper(test)
 {
     rtl_and(s, s0, dsrc2, dsrc1);
     operand_write(s, id_dest, s0);
+    rtl_update_ZFSF(s, ddest, id_dest->width);
     rtl_li(s, s0, 0);
-    rtl_set_CF(s,s0);
-    rtl_set_OF(s,s0);
+    rtl_set_CF(s, s0);
+    rtl_set_OF(s, s0);
     print_asm_template2(test);
 }
 
 static inline def_EHelper(and)
 {
-    rtl_sext(s,dsrc1,dsrc1,id_src1->width);
+    rtl_sext(s, dsrc1, dsrc1, id_src1->width);
     rtl_and(s, s0, ddest, dsrc1);
     operand_write(s, id_dest, s0);
     /*
@@ -21,9 +22,10 @@ static inline def_EHelper(and)
     rtl_msb(s,s0,ddest,id_dest->width);
     rtl_set_SF(s,s0);
     *s0=0;*/
+    rtl_update_ZFSF(s, ddest, id_dest->width);
     rtl_li(s, s0, 0);
-    rtl_set_CF(s,s0);
-    rtl_set_OF(s,s0);
+    rtl_set_CF(s, s0);
+    rtl_set_OF(s, s0);
     print_asm_template2(and);
 }
 
@@ -37,9 +39,10 @@ static inline def_EHelper(xor)
     rtl_msb(s,s0,ddest,id_dest->width);
     rtl_set_SF(s,s0);
     *s0=0;*/
+    rtl_update_ZFSF(s, ddest, id_dest->width);
     rtl_li(s, s0, 0);
-    rtl_set_CF(s,s0);
-    rtl_set_OF(s,s0);
+    rtl_set_CF(s, s0);
+    rtl_set_OF(s, s0);
     print_asm_template2(xor);
 }
 
@@ -48,8 +51,8 @@ static inline def_EHelper(or)
     rtl_or(s, s0, ddest, dsrc1);
     operand_write(s, id_dest, s0);
     rtl_li(s, s0, 0);
-    rtl_set_CF(s,s0);
-    rtl_set_OF(s,s0);
+    rtl_set_CF(s, s0);
+    rtl_set_OF(s, s0);
     print_asm_template2(or);
 }
 
