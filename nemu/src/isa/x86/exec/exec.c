6a627b7577ab147b9f3a519ab72909578b62c59d
tracer-ics2020 <tracer@njuics.org>
2020-10-14 16:25:57 +0800

    >  compile 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: 5627b174aa5f4b1fb3dbf7d7ca1f2f15     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  16:25:57 up  3:25,  1 user,  load average: 0.11, 0.09, 0.05

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -25,7 +25,7 @@ static inline def_EHelper(gp1)
         EXW(4,and,1) //////////////
         EXW(5,sub,1)//EXW(5,sub,1)?
         EMPTY(6) //////////////
-        EX(7,cmp)
+        EXW(7,cmp,1)
     }
 }
 
