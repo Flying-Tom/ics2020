ee02e055d80422b40a1263fe047f75858576165f
tracer-ics2020 <tracer@njuics.org>
2020-10-14 17:10:31 +0800

    >  compile 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: 5627b174aa5f4b1fb3dbf7d7ca1f2f15     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  17:10:32 up  4:09,  1 user,  load average: 0.00, 0.03, 0.04

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -181,7 +181,7 @@ again:
         //////////////////
         IDEX(0x30, G2E,xor)
         IDEX(0x31, G2E,xor)
-        IDEX(0x32, E2G,xor) // XOR r8,r/m8
+        IDEXW(0x32, E2G,xor,1) // XOR r8,r/m8
         IDEX(0x33, E2G,xor)
         IDEX(0x34, I2a,xor)
         IDEX(0x35, I2a,xor)
