3e09927a66398463b822c498c4aabb94aed855de
tracer-ics2020 <tracer@njuics.org>
2020-10-15 17:39:29 +0800

    >  compile 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: 3ff6b06b0f404736a7f15894e28daa5d     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  17:39:29 up  6:21,  1 user,  load average: 0.25, 0.38, 0.35

--- nemu/src/isa/x86/difftest/dut.c
+++ nemu/src/isa/x86/difftest/dut.c
@@ -12,20 +12,29 @@ static void checkregs(CPU_state *ref_r, char *s, bool *judg)
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc)
 {
-    if(ref_r->eax !=cpu.eax || ref_r->ecx !=cpu.ecx || ref_r->edx !=cpu.edx || ref_r->ebx !=cpu.ebx || ref_r->esp !=cpu.esp || ref_r->ebp !=cpu.ebp || ref_r->esi !=cpu.esi || ref_r->edi !=cpu.edi)
+    int judge=0;
+    if (ref_r->eax != cpu.eax || ref_r->ecx != cpu.ecx || ref_r->edx != cpu.edx || ref_r->ebx != cpu.ebx || ref_r->esp != cpu.esp || ref_r->ebp != cpu.ebp || ref_r->esi != cpu.esi || ref_r->edi != cpu.edi)
     {
-        printf("eax: NEMU:%d QEMU:%d\n",cpu.eax,ref_r->eax);
-        printf("ecx: NEMU:%d QEMU:%d\n",cpu.ecx,ref_r->ecx);
-        printf("edx: NEMU:%d QEMU:%d\n",cpu.edx,ref_r->edx);
-        printf("ebx: NEMU:%d QEMU:%d\n",cpu.ebx,ref_r->ebx);
-        printf("esp: NEMU:%d QEMU:%d\n",cpu.esp,ref_r->esp);
-        printf("ebp: NEMU:%d QEMU:%d\n",cpu.ebp,ref_r->ebp);
-        printf("esi: NEMU:%d QEMU:%d\n",cpu.esi,ref_r->esi);
-        printf("edi: NEMU:%d QEMU:%d\n",cpu.edi,ref_r->edi);
-        return false;
+        printf("eax: NEMU:%d QEMU:%d\n", cpu.eax, ref_r->eax);
+        printf("ecx: NEMU:%d QEMU:%d\n", cpu.ecx, ref_r->ecx);
+        printf("edx: NEMU:%d QEMU:%d\n", cpu.edx, ref_r->edx);
+        printf("ebx: NEMU:%d QEMU:%d\n", cpu.ebx, ref_r->ebx);
+        printf("esp: NEMU:%d QEMU:%d\n", cpu.esp, ref_r->esp);
+        printf("ebp: NEMU:%d QEMU:%d\n", cpu.ebp, ref_r->ebp);
+        printf("esi: NEMU:%d QEMU:%d\n", cpu.esi, ref_r->esi);
+        printf("edi: NEMU:%d QEMU:%d\n", cpu.edi, ref_r->edi);
+        judge=1;
     }
-    else
-        return true;
+
+    if (ref_r->eflags.OF != cpu.eflags.OF || ref_r->eflags.CF != cpu.eflags.CF || ref_r->eflags.ZF != cpu.eflags.ZF || ref_r->eflags.SF != cpu.eflags.SF)
+    {
+        printf("OF: NEMU:%d QEMU:%d\n", cpu.eflags.OF, ref_r->eflags.OF);
+        printf("SF: NEMU:%d QEMU:%d\n", cpu.eflags.SF, ref_r->eflags.SF);
+        printf("ZF: NEMU:%d QEMU:%d\n", cpu.eflags.ZF, ref_r->eflags.ZF);
+        printf("CF: NEMU:%d QEMU:%d\n", cpu.eflags.CF, ref_r->eflags.CF);
+        judge=1;
+    }
+    return (judge==0);
 }
 
 void isa_difftest_attach()
