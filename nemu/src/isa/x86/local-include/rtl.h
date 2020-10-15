e9041d48224aca17a506e80d8e4ef871d32b5be3
tracer-ics2020 <tracer@njuics.org>
2020-10-15 11:15:53 +0800

    >  compile 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: 3ff6b06b0f404736a7f15894e28daa5d     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  11:15:53 up  1:02,  1 user,  load average: 0.79, 0.44, 0.25

--- nemu/src/isa/x86/local-include/rtl.h
+++ nemu/src/isa/x86/local-include/rtl.h
@@ -144,7 +144,8 @@ def_rtl_setget_eflags(CF)
 static inline def_rtl(update_SF, const rtlreg_t *result, int width)
 {
     // eflags.SF <- is_sign(result[width * 8 - 1 .. 0])
-    rtl_shri(s, &cpu.eflags.SF, result, width * 8 - 1);
+    rtl_msb(s,s0,result,width);
+    rtl_set_SF(s,s0);
 }
 
 static inline def_rtl(update_ZFSF, const rtlreg_t *result, int width)
