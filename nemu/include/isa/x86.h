50df77bb12319a7cdf5691f6e9975d9d18bbe117
tracer-ics2020 <tracer@njuics.org>
2020-10-16 15:22:13 +0800

    >  compile 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: c42e707f28854655ad40954903446455     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  15:22:14 up 12 min,  1 user,  load average: 0.87, 0.48, 0.32

--- nemu/include/isa/x86.h
+++ nemu/include/isa/x86.h
@@ -43,8 +43,16 @@ typedef struct
     struct
     {
         rtlreg_t CF : 1;
+        rtlreg_t : 1;
+        rtlreg_t PF : 1;
+        rtlreg_t : 1;
+        rtlreg_t AF : 1;
+        rtlreg_t : 1;
         rtlreg_t ZF : 1;
         rtlreg_t SF : 1;
+        rtlreg_t TF : 1;
+        rtlreg_t IF : 1;
+        rtlreg_t DF : 1;
         rtlreg_t OF : 1;
     } eflags;
 } x86_CPU_state;
