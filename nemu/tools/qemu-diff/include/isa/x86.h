f2341304f83b3d1c32f78c8886db7e3ba0e6254d
tracer-ics2020 <tracer@njuics.org>
2020-10-16 16:59:58 +0800

    >  gdb 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: dfb82de5f2a94bfe8ca8325b5f0c624f     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  16:59:58 up 52 min,  1 user,  load average: 0.16, 0.22, 0.27

--- nemu/tools/qemu-diff/include/isa/x86.h
+++ nemu/tools/qemu-diff/include/isa/x86.h
@@ -6,6 +6,7 @@
 
 #include "../../../../src/isa/x86/difftest/difftest.h"
 
+/*
 union isa_gdb_regs {
   struct {
     uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
@@ -16,5 +17,31 @@ union isa_gdb_regs {
     uint32_t array[77];
   };
 };
-
+*/
+union isa_gdb_regs
+{
+    struct
+    {
+        uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+        uint32_t eip;
+        struct
+        {
+            uint32_t CF : 1;
+            uint32_t : 1;
+            uint32_t PF : 1;
+            uint32_t : 1;
+            uint32_t AF : 1;
+            uint32_t : 1;
+            uint32_t ZF : 1;
+            uint32_t SF : 1;
+            uint32_t : 3;
+            uint32_t OF : 1;
+        } eflags;
+        uint32_t cs, ss, ds, es, fs, gs;
+    };
+    struct
+    {
+        uint32_t array[77];
+    };
+};
 #endif
