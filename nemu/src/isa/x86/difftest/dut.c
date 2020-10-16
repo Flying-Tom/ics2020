eef912fa4e114d62c50984435df4a8aa2c1da44c
tracer-ics2020 <tracer@njuics.org>
2020-10-16 16:52:14 +0800

    >  compile 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: dfb82de5f2a94bfe8ca8325b5f0c624f     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  16:52:14 up 44 min,  1 user,  load average: 0.55, 0.47, 0.37

--- nemu/src/isa/x86/difftest/dut.c
+++ nemu/src/isa/x86/difftest/dut.c
@@ -12,21 +12,21 @@ static void checkregs(CPU_state *ref_r, char *s, bool *judg)
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc)
 {
-    bool* judge=0;
-    difftest_check_reg("eax",pc,ref_r->eax,cpu.eax,judge);
-    difftest_check_reg("ecx",pc,ref_r->ecx,cpu.ecx,judge);
-    difftest_check_reg("edx",pc,ref_r->edx,cpu.edx,judge);
-    difftest_check_reg("ebx",pc,ref_r->ebx,cpu.ebx,judge);
-    difftest_check_reg("esp",pc,ref_r->esp,cpu.esp,judge);
-    difftest_check_reg("ebp",pc,ref_r->ebp,cpu.ebp,judge);
-    difftest_check_reg("esi",pc,ref_r->esi,cpu.esi,judge);
-    difftest_check_reg("edi",pc,ref_r->edi,cpu.edi,judge);
+    bool judge=true;
+    difftest_check_reg("eax",pc,ref_r->eax,cpu.eax, &judge);
+    difftest_check_reg("ecx",pc,ref_r->ecx,cpu.ecx, &judge);
+    difftest_check_reg("edx",pc,ref_r->edx,cpu.edx, &judge);
+    difftest_check_reg("ebx",pc,ref_r->ebx,cpu.ebx, &judge);
+    difftest_check_reg("esp",pc,ref_r->esp,cpu.esp, &judge);
+    difftest_check_reg("ebp",pc,ref_r->ebp,cpu.ebp, &judge);
+    difftest_check_reg("esi",pc,ref_r->esi,cpu.esi, &judge);
+    difftest_check_reg("edi",pc,ref_r->edi,cpu.edi, &judge);
 
-    difftest_check_reg("CF",pc,ref_r->eflags.CF,cpu.eflags.CF,judge);
-    difftest_check_reg("ZF",pc,ref_r->eflags.ZF,cpu.eflags.ZF,judge);
-    difftest_check_reg("SF",pc,ref_r->eflags.SF,cpu.eflags.SF,judge);
-    difftest_check_reg("OF",pc,ref_r->eflags.OF,cpu.eflags.OF,judge);
-    return *judge;
+    difftest_check_reg("CF",pc,ref_r->eflags.CF,cpu.eflags.CF, &judge);
+    difftest_check_reg("ZF",pc,ref_r->eflags.ZF,cpu.eflags.ZF, &judge);
+    difftest_check_reg("SF",pc,ref_r->eflags.SF,cpu.eflags.SF, &judge);
+    difftest_check_reg("OF",pc,ref_r->eflags.OF,cpu.eflags.OF, &judge);
+    return judge;
 }
 
 void isa_difftest_attach()
