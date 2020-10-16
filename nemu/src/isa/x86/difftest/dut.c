1964d787e8a5277dd8a4fc37a18deb0cae78f5ef
tracer-ics2020 <tracer@njuics.org>
2020-10-16 19:34:30 +0800

    >  compile 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: dfb82de5f2a94bfe8ca8325b5f0c624f     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  19:34:30 up  2:52,  1 user,  load average: 0.24, 0.09, 0.02

--- nemu/src/isa/x86/difftest/dut.c
+++ nemu/src/isa/x86/difftest/dut.c
@@ -21,11 +21,11 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc)
     difftest_check_reg("ebp", pc, ref_r->ebp, cpu.ebp, &judge);
     difftest_check_reg("esi", pc, ref_r->esi, cpu.esi, &judge);
     difftest_check_reg("edi", pc, ref_r->edi, cpu.edi, &judge);
-    /*
-    difftest_check_reg("CF", pc, (temp >> 0) & 1, cpu.eflags.CF, &judge);
-    difftest_check_reg("ZF", pc, (temp >> 6) & 1, cpu.eflags.ZF, &judge);
-    difftest_check_reg("SF", pc, (temp >> 7) & 1, cpu.eflags.SF, &judge);
-    difftest_check_reg("OF", pc, (temp >> 11) & 1, cpu.eflags.OF, &judge);*/
+
+    difftest_check_reg("CF", pc, ref_r->eflags.CF, cpu.eflags.CF, &judge);
+    difftest_check_reg("ZF", pc, ref_r->eflags.ZF, cpu.eflags.ZF, &judge);
+    difftest_check_reg("SF", pc, ref_r->eflags.SF, cpu.eflags.SF, &judge);
+    difftest_check_reg("OF", pc, ref_r->eflags.OF, cpu.eflags.OF, &judge);
     return judge;
 }
 
