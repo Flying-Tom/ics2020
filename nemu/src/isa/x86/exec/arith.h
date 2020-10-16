1b53892f7f226ca3932cb55ff2ff6bbeb77b4b54
tracer-ics2020 <tracer@njuics.org>
2020-10-16 20:15:02 +0800

    >  compile 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: dfb82de5f2a94bfe8ca8325b5f0c624f     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  20:15:02 up  3:33,  1 user,  load average: 0.25, 0.16, 0.11

--- nemu/src/isa/x86/exec/arith.h
+++ nemu/src/isa/x86/exec/arith.h
@@ -62,9 +62,9 @@ static inline def_EHelper(inc)
     rtl_add(s, s0, ddest, s3);
     operand_write(s, id_dest, s0);
     rtl_update_ZFSF(s, s0, id_dest->width);
-    rtl_is_add_carry(s, s1, s0, dsrc1);
+    rtl_is_add_carry(s, s1, s0, s3);
     rtl_set_CF(s, s1);
-    rtl_is_add_overflow(s, s1, s0, ddest, dsrc1, id_dest->width);
+    rtl_is_add_overflow(s, s1, s0, ddest, s3, id_dest->width);
     rtl_set_OF(s, s1);
     print_asm_template1(inc);
 }
@@ -75,9 +75,9 @@ static inline def_EHelper(dec)
     rtl_sub(s, s0, ddest, s3);
     operand_write(s, id_dest, s0);
     rtl_update_ZFSF(s, ddest, id_dest->width);
-    rtl_is_sub_carry(s, s1, ddest, dsrc1);
+    rtl_is_sub_carry(s, s1, ddest, s3);
     rtl_set_CF(s, s1);
-    rtl_is_sub_overflow(s, s1, s0, ddest, dsrc1, id_dest->width);
+    rtl_is_sub_overflow(s, s1, s0, ddest, s3, id_dest->width);
     rtl_set_OF(s, s1);
     print_asm_template1(dec);
 }
