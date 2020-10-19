4139980b2e34a4516c65bd628fcb111dff456fc8
tracer-ics2020 <tracer@njuics.org>
2020-10-19 10:15:34 +0800

    >  compile 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: 63131f243acf45999e6056f4bb9ae7e3     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  10:15:35 up 19 min,  1 user,  load average: 0.62, 0.39, 0.41

--- nemu/src/isa/x86/difftest/difftest.h
+++ nemu/src/isa/x86/difftest/difftest.h
@@ -1,6 +1,6 @@
 #ifndef __DIFFTEST_H__
 #define __DIFFTEST_H__
 
-#define DIFFTEST_REG_SIZE (sizeof(uint32_t) * 9) // GRPs + PC
+#define DIFFTEST_REG_SIZE (sizeof(uint32_t) * 10) // GRPs + PC
 
 #endif
