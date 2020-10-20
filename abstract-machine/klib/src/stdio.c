08e9b463c03b8acf15e0589e0aac04f10edc5382
tracer-ics2020 <tracer@njuics.org>
2020-10-20 13:30:23 +0800

    >  gdb 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: d615204fcd9a44d08acc4f1e5ee30cfa     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  13:30:23 up  1:34,  1 user,  load average: 0.21, 0.14, 0.30

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,24 +5,29 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
-  return 0;
+int printf(const char *fmt, ...)
+{
+    return 0;
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  return 0;
+int vsprintf(char *out, const char *fmt, va_list ap)
+{
+    return 0;
 }
 
-int sprintf(char *out, const char *fmt, ...) {
-  return 0;
+int sprintf(char *out, const char *fmt, ...)
+{
+    return 0;
 }
 
-int snprintf(char *out, size_t n, const char *fmt, ...) {
-  return 0;
+int snprintf(char *out, size_t n, const char *fmt, ...)
+{
+    return 0;
 }
 
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  return 0;
+int vsnprintf(char *out, size_t n, const char *fmt, va_list ap)
+{
+    return 0;
 }
 
 #endif
