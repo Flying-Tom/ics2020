7056fb35f699810bad46f37a7dd190195caeeeda
tracer-ics2020 <tracer@njuics.org>
2020-10-21 21:33:15 +0800

    >  compile 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: de427148269d46b38b98d95f9dcee91b     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  21:33:15 up  7:52,  1 user,  load average: 0.13, 0.07, 0.03

--- nemu/src/device/device.c
+++ nemu/src/device/device.c
@@ -14,56 +14,69 @@ void init_i8042();
 void init_audio();
 
 void send_key(uint8_t, bool);
+void vga_update_screen();
 
 static int device_update_flag = false;
 
-static void set_device_update_flag() {
-  device_update_flag = true;
+static void set_device_update_flag()
+{
+    device_update_flag = true;
 }
 
-void device_update() {
-  if (!device_update_flag) {
-    return;
-  }
-  device_update_flag = false;
-
-  SDL_Event event;
-  while (SDL_PollEvent(&event)) {
-    switch (event.type) {
-      case SDL_QUIT:
-        nemu_state.state = NEMU_QUIT;
-        break;
-      // If a key was pressed
-      case SDL_KEYDOWN:
-      case SDL_KEYUP: {
-        uint8_t k = event.key.keysym.scancode;
-        bool is_keydown = (event.key.type == SDL_KEYDOWN);
-        send_key(k, is_keydown);
-        break;
-      }
-      default: break;
+void device_update()
+{
+    if (!device_update_flag)
+    {
+        return;
     }
-  }
+    device_update_flag = false;
+    vga_update_screen();
+    
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+        switch (event.type)
+        {
+        case SDL_QUIT:
+            nemu_state.state = NEMU_QUIT;
+            break;
+        // If a key was pressed
+        case SDL_KEYDOWN:
+        case SDL_KEYUP:
+        {
+            uint8_t k = event.key.keysym.scancode;
+            bool is_keydown = (event.key.type == SDL_KEYDOWN);
+            send_key(k, is_keydown);
+            break;
+        }
+        default:
+            break;
+        }
+    }
 }
 
-void sdl_clear_event_queue() {
-  SDL_Event event;
-  while (SDL_PollEvent(&event));
+void sdl_clear_event_queue()
+{
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+        ;
 }
 
-void init_device() {
-  init_serial();
-  init_timer();
-  init_vga();
-  init_i8042();
-  init_audio();
+void init_device()
+{
+    init_serial();
+    init_timer();
+    init_vga();
+    init_i8042();
+    init_audio();
 
-  add_alarm_handle(set_device_update_flag);
-  init_alarm();
+    add_alarm_handle(set_device_update_flag);
+    init_alarm();
 }
 #else
 
-void init_device() {
+void init_device()
+{
 }
 
-#endif	/* HAS_IOE */
+#endif /* HAS_IOE */
