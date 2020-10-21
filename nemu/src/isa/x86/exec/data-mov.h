2fb8fbbfe7b696022f066521f1c7fd41fd2082cf
tracer-ics2020 <tracer@njuics.org>
2020-10-21 08:45:40 +0800

    >  compile 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: a473cd7fda1c425fa8936309cadea445     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  08:45:40 up 17 min,  1 user,  load average: 0.47, 0.22, 0.18

--- nemu/src/isa/x86/exec/data-mov.h
+++ nemu/src/isa/x86/exec/data-mov.h
@@ -1,3 +1,4 @@
+#include <monitor/difftest.h>
 static inline def_EHelper(mov)
 {
     operand_write(s, id_dest, dsrc1);
@@ -75,6 +76,7 @@ static inline def_EHelper(cwtl)
 
 static inline def_EHelper(movsx)
 {
+    difftest_skip_ref(s);
     id_dest->width = s->isa.is_operand_size_16 ? 2 : 4;
     rtl_sext(s, ddest, dsrc1, id_src1->width);
     operand_write(s, id_dest, ddest);
