6164eb5d3e5a3b7de180dcb67ad54993261d7a37
tracer-ics2020 <tracer@njuics.org>
2020-10-21 16:24:44 +0800

    >  run 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: de427148269d46b38b98d95f9dcee91b     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  16:24:44 up  2:43,  1 user,  load average: 0.22, 0.09, 0.07

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -4,18 +4,40 @@
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-/*
-int _Printf(char *s,const char *,va_list args)
-{
-    
-    while(*fmt!='\0')
-    {
-        while(*fmt!='%')
-        *out++=*fmt++;
-        fmt++;
 
+char* num_to_str(int x)
+{
+    char *temp='\0',*out='\0';
+    int len=0;
+    while(x)
+    {
+        *temp++=(char)(x%10+'0');
+        x/=10;
+        len++;
     }
-}*/
+    while (len--)
+        *out++=*temp--;
+    return out;
+}
+int _Printf(char *out, const char *fmt, va_list args)
+{
+
+    while (*fmt != '\0')
+    {
+        while (*fmt != '%')
+            *out++ = *fmt++;
+        fmt++;
+        switch (*fmt)
+        {
+        case 'd':
+            strcat(out,num_to_str(va_arg(args, int)));
+            break;
+        case 's':
+            break;
+        }
+    }
+    return 0;
+}
 int printf(const char *fmt, ...)
 {
     return 0;
@@ -28,12 +50,12 @@ int vsprintf(char *out, const char *fmt, va_list ap)
 
 int sprintf(char *out, const char *fmt, ...)
 {
-    /*va_list ap;
+    va_list ap;
     va_start(ap,fmt);
-    int ans = _Printf(s,fmt,ap);
-    s[ans]='\0';
+    int ans = _Printf(out,fmt,ap);
+    out[ans]='\0';
     va_end(ap);
-    return ans;*/
+    return ans;
     return 0;
 }
 
