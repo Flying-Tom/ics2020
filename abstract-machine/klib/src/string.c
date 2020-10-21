878fa7ad0eed9dbc49114b2bd8c464dda1561aef
tracer-ics2020 <tracer@njuics.org>
2020-10-21 15:44:14 +0800

    >  run 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: de427148269d46b38b98d95f9dcee91b     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  15:44:14 up  2:03,  1 user,  load average: 0.05, 0.04, 0.00

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -44,9 +44,9 @@ char *strcat(char *dst, const char *src)
 
 int strcmp(const char *s1, const char *s2)
 {
-    while (*s1 && (*s1 == *s2))
-        s1++, s2++;
-    return s1 - s2;
+    while (*s1 && *s2 && *s1++ == *s2++)
+    ;
+    return *s1 - *s2;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n)
