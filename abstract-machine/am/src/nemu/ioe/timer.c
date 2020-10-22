93de9f94e400281feefca5b06074eee15fe47f30
tracer-ics2020 <tracer@njuics.org>
2020-10-22 19:51:21 +0800

    >  run 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: 3e35fcd01a364d74a5929b6529c91f1e     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  19:51:21 up 47 min,  1 user,  load average: 0.38, 0.28, 0.30

--- abstract-machine/am/src/nemu/ioe/timer.c
+++ abstract-machine/am/src/nemu/ioe/timer.c
@@ -1,15 +1,15 @@
 #include <am.h>
 #include <nemu.h>
 
-uint64_t BOOT_TIME = 0;
+//uint64_t BOOT_TIME = 0;
 void __am_timer_init()
 {
-    BOOT_TIME = inl(RTC_ADDR);
+    //BOOT_TIME = inl(RTC_ADDR);
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime)
 {
-    uptime->us = inl(RTC_ADDR) - BOOT_TIME;
+    uptime->us = inl(RTC_ADDR) - 0;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc)
