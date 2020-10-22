9b415ff3894b02f39acca044e94fc84688c41ea8
tracer-ics2020 <tracer@njuics.org>
2020-10-22 12:18:37 +0800

    >  compile 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: c3c43807cdd844bea82659ac2b35c511     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  12:18:37 up  1:54,  1 user,  load average: 0.62, 0.42, 0.37

--- nemu/src/isa/x86/exec/data-mov.h
+++ nemu/src/isa/x86/exec/data-mov.h
@@ -97,15 +97,15 @@ static inline def_EHelper(movsb)
     rtl_li(s,s3,id_dest->width);
     if (id_dest->width == 1)
     {
-        rtl_lr(s, s0, R_ESI, 1);
-        rtl_sr(s, R_EDI, s0, 1);
+        rtl_lr(s, s0, R_ESI, 2);
+        rtl_sr(s, R_EDI, s0, 2);
     }
-    rtl_lr(s, s0, R_ESI, id_dest->width);
+    rtl_lr(s, s0, R_ESI, 4);
     rtl_add(s,s0,s0,s3);
-    rtl_sr(s, R_ESI, s0, id_dest->width);
-    rtl_lr(s, s0, R_EDI, id_dest->width);
+    rtl_sr(s, R_ESI, s0, 4);
+    rtl_lr(s, s0, R_EDI, 4);
     rtl_add(s,s0,s0,s3);
-    rtl_sr(s, R_EDI, s0, id_dest->width);
+    rtl_sr(s, R_EDI, s0, 4);
     print_asm_template1(movsb);
 }
 
