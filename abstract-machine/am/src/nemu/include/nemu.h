bc4b55171006e6a889c891b33d4f38ab1d5404b9
tracer-ics2020 <tracer@njuics.org>
2020-10-22 21:03:10 +0800

    >  run 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: 3e35fcd01a364d74a5929b6529c91f1e     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  21:03:11 up  1:59,  1 user,  load average: 0.01, 0.16, 0.21

--- abstract-machine/am/src/nemu/include/nemu.h
+++ abstract-machine/am/src/nemu/include/nemu.h
@@ -4,12 +4,14 @@
 #include <klib-macros.h>
 #include "x86.h" // "x86.h", "mips32.h", ...
 
+/*
 #ifndef __ISA_X86__
 #define __ISA_X86__
 #endif
 #ifndef __ARCH_X86_NEMU
 #define __ARCH_X86_NEMU
 #endif
+*/
 
 #if defined(__ISA_X86__)
 # define nemu_trap(code) asm volatile (".byte 0xd6" : :"a"(code))
