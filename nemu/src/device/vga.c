d088de484d3d1c57b95d007b125ffc55fb6e4afa
tracer-ics2020 <tracer@njuics.org>
2020-10-28 00:37:56 +0800

    >  compile 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: 7cc0e0977a4246978e41161bbd85ebf6     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  00:37:57 up  5:49,  1 user,  load average: 0.23, 0.42, 0.30

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -43,10 +43,10 @@ void vga_update_screen()
 {
     // TODO: call `update_screen()` when the sync register is non-zero,
     // then zero out the sync register
-    if (paddr_read(VGACTL_MMIO + 4, 4))
+    if (paddr_read(VGACTL_MMIO + 4, 1))
     {
         update_screen();
-        paddr_write(VGACTL_MMIO + 4, 0, 4);
+        paddr_write(VGACTL_MMIO + 4, 0, 1);
     }
 }
 
