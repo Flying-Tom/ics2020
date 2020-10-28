a259b288bd33043cd52eef21d032ae2cb838a6cb
tracer-ics2020 <tracer@njuics.org>
2020-10-28 10:18:36 +0800

    >  run 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: 631876d8355248b3ad7f1c273877ff9c     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  10:18:36 up  1:26,  1 user,  load average: 0.87, 0.65, 0.45

--- abstract-machine/am/src/nemu/ioe/gpu.c
+++ abstract-machine/am/src/nemu/ioe/gpu.c
@@ -47,8 +47,9 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl)
     for (int j = 0; j < h && y + j < H; j++)
     {
         for (int i = 0; i < cp_bytes; i++)
-            *(fb + (y + j) * W + x + i) = *(pixels+i);
-        //memcpy(&fb[(y + j) * W + x], pixels, cp_bytes);
+        {    //*(fb + (y + j) * W + x + i) = *(pixels+i);
+            memcpy(&fb[(y + j) * W + x], pixels, cp_bytes);
+        }
         pixels += w;
     }
 }
