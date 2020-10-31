d0782cba0ed1a271d38229f8972ecdd600fdab99
tracer-ics2020 <tracer@njuics.org>
2020-10-31 21:39:22 +0800

    >  compile 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: 7db1923bf4f74dea9d90d91e962bdca7     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  21:39:23 up  6:38,  1 user,  load average: 0.23, 0.20, 0.12

--- nemu/src/isa/x86/exec/logic.h
+++ nemu/src/isa/x86/exec/logic.h
@@ -82,23 +82,23 @@ static inline def_EHelper(not )
 
 static inline def_EHelper(rol)
 {
-    rtl_mv(s, s0, dsrc1);
-    id_src1->width = 1;
-    while (*s0)
+    rtl_mv(s, s0, ddest);
+    rtl_mv(s, s1, dsrc1);
+    while (*s1)
     {
-        rtl_msb(s, s1, ddest, id_dest->width);
-        rtl_shli(s, ddest, ddest, 1);
-        rtl_add(s, s2, s2, s1);
-        rtl_subi(s, s0, s0, 1);
+        rtl_msb(s, s2, s0, id_dest->width);
+        rtl_shli(s, s0, s0, 1);
+        rtl_add(s, s0, s0, s2);
+        rtl_subi(s, s1, s1, 1);
     }
     if (*dsrc1 == 1)
     {
         rtl_msb(s, s1, ddest, id_dest->width);
-        rtl_get_CF(s,s2);
+        rtl_get_CF(s, s2);
         rtl_li(s, s3, s1 != s2 ? 1 : 0);
-        rtl_set_OF(s,s3);
+        rtl_set_OF(s, s3);
     }
-    operand_write(s, id_dest, ddest);
+    operand_write(s, id_dest, s0);
     print_asm_template2(rol);
 }
 
