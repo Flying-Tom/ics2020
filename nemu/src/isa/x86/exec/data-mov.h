0999556e286c09cffafee7ea9db1b99f2c248146
tracer-ics2020 <tracer@njuics.org>
2020-10-31 16:54:31 +0800

    >  compile 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: 7db1923bf4f74dea9d90d91e962bdca7     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  16:54:31 up  1:53,  1 user,  load average: 0.14, 0.11, 0.03

--- nemu/src/isa/x86/exec/data-mov.h
+++ nemu/src/isa/x86/exec/data-mov.h
@@ -96,9 +96,9 @@ static inline def_EHelper(movzx)
 
 static inline def_EHelper(movsb)
 {
+    assert(s->width == 1 || s->width == 2 || s->width == 4 );
     rtl_lr(s, s0, R_ESI, 4);
-    rtl_sr(s, R_EDI, s0, 4);
-    rtl_addi(s, s0, s0, 4);
+    rtl_addi(s, s0, s0, s->width);
     rtl_sr(s, R_ESI, s0, 4);
     rtl_sr(s, R_EDI, s0, 4);
     print_asm_template1(movsb);
