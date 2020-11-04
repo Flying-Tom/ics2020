0a3504aee8a483cd53c6b854710fb764e1f222e1
tracer-ics2020 <tracer@njuics.org>
2020-11-05 02:29:28 +0800

    >  run 191250004    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f267802d9e6243129bd62cb29ae283f2            Boot ID: 8b73975678684f6994d01f4d66e35569     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  02:29:28 up  6:10,  1 user,  load average: 1.05, 0.37, 0.28

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -103,17 +103,15 @@ void *memcpy(void *out, const void *in, size_t n)
 
 int memcmp(const void *s1, const void *s2, size_t n)
 {
-    char *str1 = (char*) s1;
-    char *str2 = (char*) s2;
-    while (n--)
+    char *str1 = (char *)s1;
+    char *str2 = (char *)s2;
+
+    while (*str1 == *str2 && n-- > 0)
     {
-        while (*str1 == *str2)
-        {
-            if (*str1 == '\0')
-                return 0;
-            str1++;
-            str2++;
-        }
+        if (*str1 == '\0')
+            return 0;
+        str1++;
+        str2++;
     }
     if (*str1 > *str2)
         return 1;
