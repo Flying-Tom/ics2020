4dc4073544d190f7f80094255d669615266447bd
tracer-ics2020 <tracer@njuics.org>
2020-11-25 03:31:17 +0800

    >  compile 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: ec123108b0ec4e71beee7360f9b80954   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  03:31:17 up  1:55,  0 users,  load average: 0.11, 0.25, 0.35

--- nemu/src/isa/x86/exec/logic.h
+++ nemu/src/isa/x86/exec/logic.h
@@ -109,6 +109,7 @@ static inline def_EHelper(ror)
     rtl_mv(s, s0, ddest);
     rtl_mv(s, s1, dsrc1);
     *s1 %= 8 * id_dest->width;
+    printf("s1:%d iddestwidth:%d",*s1,id_dest->width);
     while (*s1)
     {
         rtl_andi(s, s2, ddest, 1);
