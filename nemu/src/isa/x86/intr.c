1748b9391ebb9c07570b25bed3eecb799e325628
tracer-ics2020 <tracer@njuics.org>
2020-11-26 01:35:38 +0800

    >  compile 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: d223e74c0b554a83a0eb3e3ef6496de1   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  01:35:39 up  9:54,  1 user,  load average: 0.01, 0.03, 0.05

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -1,17 +1,23 @@
 #include <cpu/exec.h>
 #include "local-include/rtl.h"
 
-void raise_intr(DecodeExecState *s, uint32_t NO, vaddr_t ret_addr) {
-  /* TODO: Trigger an interrupt/exception with ``NO''.
+void raise_intr(DecodeExecState *s, uint32_t NO, vaddr_t ret_addr)
+{
+    /* TODO: Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
-  /*GateDesc32 gate;
+    /*GateDesc32 gate;
   vaddr_t gate_addr = cpu.IDTR.base + NO * 0x8;
   gate.*/
-
-  TODO();
+    rtl_push(s,&cpu.EFLAGS);
+    cpu.eflags.IF = 0;
+    cpu.eflags.TF = 0;
+    rtl_push(s,&cpu.cs);
+    rtl_push(s,&cpu.pc);
+    TODO();
 }
 
-void query_intr(DecodeExecState *s) {
-  TODO();
+void query_intr(DecodeExecState *s)
+{
+    TODO();
 }
