61a8df382b93d7b21fb52ea3822e756474cac4bd
tracer-ics2020 <tracer@njuics.org>
2020-11-27 06:28:43 +0800

    >  compile 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 86c26788e16541af81f8f1e31a415bd2   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  06:28:43 up  1:16,  1 user,  load average: 0.62, 0.14, 0.05

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -7,13 +7,18 @@ void raise_intr(DecodeExecState *s, uint32_t NO, vaddr_t ret_addr)
    * That is, use ``NO'' to index the IDT.
    */
     //GateDesc32 gate;
-  /*vaddr_t gate_addr = cpu.IDTR.base + NO * 0x8;
-  gate.*/
+    /*vaddr_t gate_addr = cpu.IDTR.base + NO * 0x8;
+    gate.*/
+    vaddr_t gate_addr = cpu.IDTR.base + NO * 0x8;
+    vaddr_t gate_selector = vaddr_read(gate_addr, 4);
+    vaddr_t gate_offset = vaddr_read(gate_addr + 4, 4);
     rtl_push(s, &cpu.EFLAGS);
     cpu.eflags.IF = 0;
     cpu.eflags.TF = 0;
     rtl_push(s, &cpu.cs);
     rtl_push(s, &cpu.pc);
+    cpu.cs = gate_selector;
+    rtl_j(s,gate_offset);
 }
 
 void query_intr(DecodeExecState *s)
