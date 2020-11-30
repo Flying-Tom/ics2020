4ad73820c642f5f6c5e94db4146e9ae649072623
tracer-ics2020 <tracer@njuics.org>
2020-11-30 19:42:18 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: f4ef12c3ba8043709f8add3c62c2a7a3   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  19:42:18 up 10:12,  0 users,  load average: 0.00, 0.03, 0.01

--- abstract-machine/am/src/nemu/isa/x86/cte.c
+++ abstract-machine/am/src/nemu/isa/x86/cte.c
@@ -21,11 +21,29 @@ Context *__am_irq_handle(Context *c)
 		Event ev = {0};
 		switch (c->irq)
 		{
-		//case 1:
-		//	ev.event = EVENT_YIELD;
-		//	break;
-		default:
+		case 0:
+			ev.event = EVENT_NULL;
+			break;
+		case 1:
 			ev.event = EVENT_YIELD;
+			break;
+		case 2:
+			ev.event = EVENT_SYSCALL;
+			break;
+		case 3:
+			ev.event = EVENT_PAGEFAULT;
+			break;
+		case 4:
+			ev.event = EVENT_ERROR;
+			break;
+		case 5:
+			ev.event = EVENT_IRQ_TIMER;
+			break;
+		case 6:
+			ev.event = EVENT_IRQ_IODEV;
+			break;
+		default:
+			ev.event = EVENT_ERROR;
 			break;
 		}
 
