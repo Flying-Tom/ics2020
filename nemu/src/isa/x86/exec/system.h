0376da623d38ba6cd902027c4bd4fdd4507e023c
tracer-ics2020 <tracer@njuics.org>
2020-11-30 16:01:46 +0800

    >  compile 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: f4ef12c3ba8043709f8add3c62c2a7a3   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  16:01:46 up  6:31,  0 users,  load average: 0.07, 0.04, 0.00

--- nemu/src/isa/x86/exec/system.h
+++ nemu/src/isa/x86/exec/system.h
@@ -4,10 +4,10 @@ void raise_intr(DecodeExecState *, uint32_t, vaddr_t);
 
 static inline def_EHelper(lidt)
 {
-    printf("mbase:%x\n",*s->isa.mbase);
-    printf("moff:%x\n",s->isa.moff);
-    rtl_host_lm(s, s0, s->isa.mbase + s->isa.moff, 2);
-    rtl_host_lm(s, s1, s->isa.mbase + s->isa.moff + 2, 4);
+    //printf("mbase:%x\n",*s->isa.mbase);
+    //printf("moff:%x\n",s->isa.moff);
+    rtl_lm(s, s0, s->isa.mbase , s->isa.moff, 2);
+    rtl_lm(s, s1, s->isa.mbase , s->isa.moff + 2, 4);
     cpu.IDTR.limit = *s0;
     cpu.IDTR.base = *s1;
     print_asm_template1(lidt);
