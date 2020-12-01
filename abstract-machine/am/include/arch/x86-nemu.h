2366f32bb073ffd2f5aaf462b5ea9977045278ee
tracer-ics2020 <tracer@njuics.org>
2020-12-01 18:22:38 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 088c158c05bd478599c2168f69bbdfe2   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  18:22:38 up  8:52,  1 user,  load average: 0.08, 0.04, 0.01

--- abstract-machine/am/include/arch/x86-nemu.h
+++ abstract-machine/am/include/arch/x86-nemu.h
@@ -4,7 +4,7 @@
 struct Context
 {
   void *cr3;
-  uintptr_t temp, edi, esi, ebp, esp, ebx, edx, ecx, eax;
+  uintptr_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
   int irq;
   uintptr_t eip, cs, eflags;
 };
