75812a0fd9d820b06560198410da2d84f56831d7
tracer-ics2020 <tracer@njuics.org>
2020-12-01 20:48:28 +0800

    >  compile 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 088c158c05bd478599c2168f69bbdfe2   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  20:48:28 up 11:18,  1 user,  load average: 0.03, 0.01, 0.00

--- nemu/src/isa/x86/local-include/rtl.h
+++ nemu/src/isa/x86/local-include/rtl.h
@@ -46,16 +46,16 @@ static inline def_rtl(push, const rtlreg_t *src1)
 {
     // esp <- esp - 4
     // M[esp] <- src1
-    rtl_subi(s, &cpu.esp, &cpu.esp, 4);
-    rtl_sm(s, &cpu.esp, 0, src1, 4);
+    rtl_subi(s, &reg_l(R_ESP), &reg_l(R_ESP), 4);
+    rtl_sm(s, &reg_l(R_ESP), 0, src1, 4);
 }
 
 static inline def_rtl(pop, rtlreg_t *dest)
 {
     // dest <- M[esp]
     // esp <- esp + 4
-    rtl_lm(s, dest, &cpu.esp, 0, 4);
-    rtl_addi(s, &cpu.esp, &cpu.esp, 4);
+    rtl_lm(s, dest, &reg_l(R_ESP), 0, 4);
+    rtl_addi(s, &reg_l(R_ESP), &reg_l(R_ESP), 4);
 }
 
 static inline def_rtl(is_sub_overflow, rtlreg_t *dest,
