4715a4e17ed0894981df46fb6357d482004862f9
tracer-ics2020 <tracer@njuics.org>
2020-12-01 13:32:35 +0800

    >  compile 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 088c158c05bd478599c2168f69bbdfe2   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  13:32:35 up  4:02,  0 users,  load average: 0.02, 0.04, 0.06

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -7,13 +7,13 @@ void raise_intr(DecodeExecState *s, uint32_t NO, vaddr_t ret_addr)
     * That is, use ``NO'' to index the IDT.
     */
     assert(NO <= cpu.IDTR.limit);
-    printf("%x\n", NO);
+    //printf("%x\n", NO);
     rtl_push(s, &cpu.EFLAGS);
     rtl_push(s, &cpu.cs);
     rtl_push(s, &ret_addr);
     vaddr_t offset_lo = vaddr_read(cpu.IDTR.base + NO * 8, 4) & 0x0000ffff;
     vaddr_t offset_hi = vaddr_read(cpu.IDTR.base + NO * 8 + 4, 4) & 0xffff0000;
-    printf("%x\n", offset_hi | offset_lo);
+    //printf("%x\n", offset_hi | offset_lo);
     rtl_j(s, offset_hi | offset_lo);
 }
 
