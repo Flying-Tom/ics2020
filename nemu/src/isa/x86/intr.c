2ef9b6feec78e50c1a56dc5d26ec723d887c6730
tracer-ics2020 <tracer@njuics.org>
2020-12-01 13:29:12 +0800

    >  compile 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 088c158c05bd478599c2168f69bbdfe2   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  13:29:13 up  3:59,  0 users,  load average: 0.08, 0.07, 0.07

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -14,6 +14,7 @@ void raise_intr(DecodeExecState *s, uint32_t NO, vaddr_t ret_addr)
     vaddr_t offset_lo = vaddr_read(cpu.IDTR.base + NO * 8, 4) & 0x0000ffff;
     vaddr_t offset_hi = vaddr_read(cpu.IDTR.base + NO * 8 + 4, 4) & 0xffff0000;
     printf("%x\n", offset_hi | offset_lo);
+    rtl_push(s, &cpu.esp);
     rtl_j(s, offset_hi | offset_lo);
 }
 
