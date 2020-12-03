b57b61219da9f7949da013c84cc18aa2a509cd2c
tracer-ics2020 <tracer@njuics.org>
2020-12-03 14:05:48 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: eca06854b0a44200bf53818dda7eace1   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  14:05:48 up  4:35,  0 users,  load average: 0.02, 0.07, 0.03

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,24 +1,34 @@
 #include <proc.h>
 #include <elf.h>
+//#include "ramdisk.c"
 
 #ifdef __LP64__
-# define Elf_Ehdr Elf64_Ehdr
-# define Elf_Phdr Elf64_Phdr
+#define Elf_Ehdr Elf64_Ehdr
+#define Elf_Phdr Elf64_Phdr
 #else
-# define Elf_Ehdr Elf32_Ehdr
-# define Elf_Phdr Elf32_Phdr
+#define Elf_Ehdr Elf32_Ehdr
+#define Elf_Phdr Elf32_Phdr
 #endif
 
-static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  //Elf_Ehdr elf;
-  
-  return 0;
+extern int ramdisk_read(void *, size_t, size_t);
+
+static uintptr_t loader(PCB *pcb, const char *filename)
+{
+    //TODO();
+    Elf_Ehdr elf;
+    ramdisk_read((void *)&elf, 0, sizeof(elf));
+    //Elf_Phdr phdr[elf.e_phnum];
+    /*for (size_t i = 0; i < elf.e_phnum; i++)
+    {
+        ramdisk_read(void(*) phdr[i].p_vadrr, 0, elf.e_phentsize);
+        if (phdr[i].p_type == PT_LOAD)
+    }*/
+    return 0;
 }
 
-void naive_uload(PCB *pcb, const char *filename) {
-  uintptr_t entry = loader(pcb, filename);
-  Log("Jump to entry = %p", entry);
-  ((void(*)())entry) ();
+void naive_uload(PCB *pcb, const char *filename)
+{
+    uintptr_t entry = loader(pcb, filename);
+    Log("Jump to entry = %p", entry);
+    ((void (*)())entry)();
 }
-
