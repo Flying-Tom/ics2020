cb7eb42204cf3863b5a53af3804519fc64743c8c
tracer-ics2020 <tracer@njuics.org>
2020-12-03 19:14:37 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: eca06854b0a44200bf53818dda7eace1   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  19:14:37 up  9:44,  0 users,  load average: 0.12, 0.04, 0.01

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -34,7 +34,7 @@ static uintptr_t loader(PCB *pcb, const char *filename)
             memset((void *)(phdr[i].p_vaddr + phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz);
         }
     }
-    return elf.e_entry;
+    return -1;
 }
 
 void naive_uload(PCB *pcb, const char *filename)
