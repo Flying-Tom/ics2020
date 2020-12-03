a93886c1a724aa02fd72cd065fef3eaa34bd56ae
tracer-ics2020 <tracer@njuics.org>
2020-12-03 19:41:55 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: eca06854b0a44200bf53818dda7eace1   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  19:41:55 up 10:11,  0 users,  load average: 0.24, 0.12, 0.03

--- abstract-machine/am/src/nemu/isa/x86/cte.c
+++ abstract-machine/am/src/nemu/isa/x86/cte.c
@@ -15,61 +15,64 @@ void __am_vecnull();
 
 Context *__am_irq_handle(Context *c)
 {
-	if (user_handler)
-	{
-		Event ev = {0};
-		switch (c->irq)
-		{
-		case 0x81:
-			ev.event = EVENT_YIELD;
-			break;
-		default:
-			ev.event = EVENT_ERROR;
-			break;
-		}
-		c = user_handler(ev, c);
-		assert(c != NULL);
-	}
-	return c;
+    if (user_handler)
+    {
+        Event ev = {0};
+        switch (c->irq)
+        {
+        case 0x80:
+            ev.event = EVENT_SYSCALL;
+            break;
+        case 0x81:
+            ev.event = EVENT_YIELD;
+            break;
+        default:
+            ev.event = EVENT_ERROR;
+            break;
+        }
+        c = user_handler(ev, c);
+        assert(c != NULL);
+    }
+    return c;
 }
 
 bool cte_init(Context *(*handler)(Event, Context *))
 {
-	static GateDesc32 idt[NR_IRQ];
+    static GateDesc32 idt[NR_IRQ];
 
-	// initialize IDT
-	for (unsigned int i = 0; i < NR_IRQ; i++)
-	{
-		idt[i] = GATE32(STS_TG, KSEL(SEG_KCODE), __am_vecnull, DPL_KERN);
-	}
+    // initialize IDT
+    for (unsigned int i = 0; i < NR_IRQ; i++)
+    {
+        idt[i] = GATE32(STS_TG, KSEL(SEG_KCODE), __am_vecnull, DPL_KERN);
+    }
 
-	// ----------------------- interrupts ----------------------------
-	idt[32] = GATE32(STS_IG, KSEL(SEG_KCODE), __am_irq0, DPL_KERN);
-	// ---------------------- system call ----------------------------
-	idt[0x80] = GATE32(STS_TG, KSEL(SEG_KCODE), __am_vecsys, DPL_USER);
-	idt[0x81] = GATE32(STS_TG, KSEL(SEG_KCODE), __am_vectrap, DPL_KERN);
+    // ----------------------- interrupts ----------------------------
+    idt[32] = GATE32(STS_IG, KSEL(SEG_KCODE), __am_irq0, DPL_KERN);
+    // ---------------------- system call ----------------------------
+    idt[0x80] = GATE32(STS_TG, KSEL(SEG_KCODE), __am_vecsys, DPL_USER);
+    idt[0x81] = GATE32(STS_TG, KSEL(SEG_KCODE), __am_vectrap, DPL_KERN);
 
-	set_idt(idt, sizeof(idt));
+    set_idt(idt, sizeof(idt));
 
-	// register event handler
-	user_handler = handler;
+    // register event handler
+    user_handler = handler;
 
-	return true;
+    return true;
 }
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg)
 {
-	return NULL;
+    return NULL;
 }
 
 void yield()
 {
-	asm volatile("int $0x81");
+    asm volatile("int $0x81");
 }
 
 bool ienabled()
 {
-	return false;
+    return false;
 }
 
 void iset(bool enable)
