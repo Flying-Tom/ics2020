7325aea13f8e1e68b880a931235a3f5e3314e63a
tracer-ics2020 <tracer@njuics.org>
2020-12-03 14:31:07 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: eca06854b0a44200bf53818dda7eace1   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  14:31:07 up  5:00,  0 users,  load average: 0.13, 0.04, 0.01

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -16,9 +16,9 @@ static uintptr_t loader(PCB *pcb, const char *filename)
 {
     //TODO();
     Elf_Ehdr elf;
+    TODO();
     ramdisk_read((void *)&elf, 0, sizeof(Elf_Ehdr));
     Elf_Phdr phdr[elf.e_phnum];
-    TODO();
     for (size_t i = 0; i < elf.e_phnum; i++)
     {
         ramdisk_read((void *)&phdr[i], 0, elf.e_phentsize);
