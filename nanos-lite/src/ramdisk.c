ca8f098e7093cc03cbedd69312021754fbfa0377
tracer-ics2020 <tracer@njuics.org>
2020-12-03 14:42:41 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: eca06854b0a44200bf53818dda7eace1   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  14:42:42 up  5:12,  0 users,  load average: 0.00, 0.00, 0.00

--- nanos-lite/src/ramdisk.c
+++ nanos-lite/src/ramdisk.c
@@ -10,24 +10,31 @@ extern uint8_t ramdisk_end;
  */
 
 /* read `len' bytes starting from `offset' of ramdisk into `buf' */
-size_t ramdisk_read(void *buf, size_t offset, size_t len) {
-  assert(offset + len <= RAMDISK_SIZE);
-  memcpy(buf, &ramdisk_start + offset, len);
-  return len;
+size_t ramdisk_read(void *buf, size_t offset, size_t len)
+{
+    printf("len:%d\n",len);
+    printf("offset:%d\n",len);
+    printf("RAMDISK_SIZE:%d\n",RAMDISK_SIZE);
+    assert(offset + len <= RAMDISK_SIZE);
+    memcpy(buf, &ramdisk_start + offset, len);
+    return len;
 }
 
 /* write `len' bytes starting from `buf' into the `offset' of ramdisk */
-size_t ramdisk_write(const void *buf, size_t offset, size_t len) {
-  assert(offset + len <= RAMDISK_SIZE);
-  memcpy(&ramdisk_start + offset, buf, len);
-  return len;
+size_t ramdisk_write(const void *buf, size_t offset, size_t len)
+{
+    assert(offset + len <= RAMDISK_SIZE);
+    memcpy(&ramdisk_start + offset, buf, len);
+    return len;
 }
 
-void init_ramdisk() {
-  Log("ramdisk info: start = %p, end = %p, size = %d bytes",
-      &ramdisk_start, &ramdisk_end, RAMDISK_SIZE);
+void init_ramdisk()
+{
+    Log("ramdisk info: start = %p, end = %p, size = %d bytes",
+        &ramdisk_start, &ramdisk_end, RAMDISK_SIZE);
 }
 
-size_t get_ramdisk_size() {
-  return RAMDISK_SIZE;
+size_t get_ramdisk_size()
+{
+    return RAMDISK_SIZE;
 }
