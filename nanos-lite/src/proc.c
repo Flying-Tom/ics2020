003ca6db8bcf193b09ed4a5e663733155e86403d
tracer-ics2020 <tracer@njuics.org>
2020-12-03 14:22:36 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: eca06854b0a44200bf53818dda7eace1   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  14:22:36 up  4:52,  0 users,  load average: 0.29, 0.10, 0.03

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -1,33 +1,39 @@
 #include <proc.h>
 
 #define MAX_NR_PROC 4
+extern void naive_uload(PCB *, const char *);
 
 static PCB pcb[MAX_NR_PROC] __attribute__((used)) = {};
 static PCB pcb_boot = {};
 PCB *current = NULL;
 
-void switch_boot_pcb() {
-  current = &pcb_boot;
+void switch_boot_pcb()
+{
+    current = &pcb_boot;
 }
 
-void hello_fun(void *arg) {
-  int j = 1;
-  while (1) {
-    Log("Hello World from Nanos-lite with arg '%p' for the %dth time!", (uintptr_t)arg, j);
-    j ++;
-    yield();
-  }
+void hello_fun(void *arg)
+{
+    int j = 1;
+    while (1)
+    {
+        Log("Hello World from Nanos-lite with arg '%p' for the %dth time!", (uintptr_t)arg, j);
+        j++;
+        yield();
+    }
 }
 
-void init_proc() {
-  switch_boot_pcb();
+void init_proc()
+{
+    switch_boot_pcb();
 
-  Log("Initializing processes...");
+    Log("Initializing processes...");
 
-  // load program here
-
+    // load program here
+    //naive_uload(NULL, NULL);
 }
 
-Context* schedule(Context *prev) {
-  return NULL;
+Context *schedule(Context *prev)
+{
+    return NULL;
 }
