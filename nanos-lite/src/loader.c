fed57a677c953bee63df7d296558222b048c5925
tracer-ics2020 <tracer@njuics.org>
2020-12-03 17:47:37 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: eca06854b0a44200bf53818dda7eace1   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  17:47:37 up  8:17,  0 users,  load average: 0.07, 0.07, 0.02

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -14,27 +14,27 @@ extern int ramdisk_read(void *, size_t, size_t);
 
 static uintptr_t loader(PCB *pcb, const char *filename)
 {
-    Elf_Ehdr elf;
-    ramdisk_read((void *)&elf, 0, sizeof(Elf_Ehdr));
-    Elf_Phdr phdr[elf.e_phnum];
+	Elf_Ehdr elf;
+	ramdisk_read((void *)&elf, 0, sizeof(Elf_Ehdr));
+	Elf_Phdr phdr[elf.e_phnum];
 
-	assert(elf.e_phentsize==sizeof(Elf_Phdr));
-	
-    for (size_t i = 0; i < elf.e_phnum; i++)
-    {
-        ramdisk_read((void *)&phdr[i], i * elf.e_phentsize, elf.e_phentsize);
-        if (phdr[i].p_type == PT_LOAD)
-        {
-            ramdisk_read((void *)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_memsz);
-            memset((void *)(phdr[i].p_vaddr + phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz);
-        }
-    }
-    return 0;
+	assert(elf.e_phentsize == sizeof(Elf_Phdr));
+
+	for (size_t i = 0; i < elf.e_phnum; i++)
+	{
+		ramdisk_read((void *)&phdr[i], elf.e_entry + i * elf.e_phentsize, elf.e_phentsize);
+		if (phdr[i].p_type == PT_LOAD)
+		{
+			ramdisk_read((void *)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_memsz);
+			memset((void *)(phdr[i].p_vaddr + phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz);
+		}
+	}
+	return 0;
 }
 
 void naive_uload(PCB *pcb, const char *filename)
 {
-    uintptr_t entry = loader(pcb, filename);
-    Log("Jump to entry = %p", entry);
-    ((void (*)())entry)();
+	uintptr_t entry = loader(pcb, filename);
+	Log("Jump to entry = %p", entry);
+	((void (*)())entry)();
 }
