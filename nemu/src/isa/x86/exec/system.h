5ea8598bf416a1be1d7305b06deaa781bef44b8e
tracer-ics2020 <tracer@njuics.org>
2020-12-03 21:21:03 +0800

    >  compile 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: eca06854b0a44200bf53818dda7eace1   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  21:21:03 up 11:50,  0 users,  load average: 0.14, 0.09, 0.02

--- nemu/src/isa/x86/exec/system.h
+++ nemu/src/isa/x86/exec/system.h
@@ -39,14 +39,7 @@ static inline def_EHelper(int)
 
 static inline def_EHelper(iret)
 {
-    int temp=10;
-    while(temp--)
-    {
-        rtl_pop(s, s0);
-        printf("%x\n",*s0);
-    }
-    rtl_j(s, *s0);
-    rtl_pop(s, &cpu.cs);
+    rtl_pop(s, &s->seq_pc);
     rtl_pop(s, &cpu.EFLAGS);
     print_asm("iret");
 
