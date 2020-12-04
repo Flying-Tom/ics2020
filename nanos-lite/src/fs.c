4107f81886897daa1abbbe0cabfab589abd82a10
tracer-ics2020 <tracer@njuics.org>
2020-12-04 22:16:15 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 92e439a860dc492bbd83cc78373c06e3   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  22:16:15 up  8:37,  1 user,  load average: 0.00, 0.02, 0.03

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -66,9 +66,12 @@ size_t fs_read(int fd, void *buf, size_t len)
 {
     assert(fd >= 0 && fd < FILE_NUM);
     static size_t offset;
-    printf("Checkpoint\n");
-    offset = ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+    if (file_table[fd].read == NULL)
+        offset = ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+    else
+        offset = ramdisk_read(buf, file_table[fd].open_offset, len);
     file_table[fd].open_offset += len;
+
     return offset;
 }
 size_t fs_write(int fd, const void *buf, size_t len)
