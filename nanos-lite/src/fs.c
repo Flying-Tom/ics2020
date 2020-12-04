649117700637014bd76b40200fa28eb4605f8f0d
tracer-ics2020 <tracer@njuics.org>
2020-12-05 01:56:32 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 92e439a860dc492bbd83cc78373c06e3   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  01:56:32 up 12:17,  1 user,  load average: 0.33, 0.36, 0.72

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -79,6 +79,7 @@ size_t fs_write(int fd, const void *buf, size_t len)
     if (file_table[fd].open_offset + len >= file_table[fd].size)
         offset = file_table[fd].size - file_table[fd].open_offset;
     offset = ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, offset);
+    file_table[fd].open_offset += offset;
     return offset;
 }
 
