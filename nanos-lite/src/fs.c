d4d1e569737de8d7025c6848bb5a0a67165d3481
tracer-ics2020 <tracer@njuics.org>
2020-12-05 01:35:37 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 92e439a860dc492bbd83cc78373c06e3   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  01:35:37 up 11:56,  1 user,  load average: 0.99, 0.88, 1.05

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -67,8 +67,6 @@ size_t fs_read(int fd, void *buf, size_t len)
 {
     assert(fd >= 0 && fd < FILE_NUM);
     size_t offset = len;
-    if (file_table[fd].size && file_table[fd].open_offset + len > file_table[fd].size)
-        offset = file_table[fd].size - file_table[fd].open_offset;
     offset = ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, offset);
     file_table[fd].open_offset += len;
     return offset;
@@ -78,8 +76,6 @@ size_t fs_write(int fd, const void *buf, size_t len)
 {
     assert(fd >= 0 && fd < FILE_NUM);
     size_t offset = len;
-    if (file_table[fd].size && file_table[fd].open_offset + len > file_table[fd].size)
-        offset = file_table[fd].size - file_table[fd].open_offset;
     offset = ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, offset);
     file_table[fd].open_offset += len;
     return offset;
--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -95,7 +95,7 @@ int _close(int fd)
 
 off_t _lseek(int fd, off_t offset, int whence)
 {
-    return _syscall_(SYS_lseek, fd, (intptr_t)offset, whence);
+    return _syscall_(SYS_lseek, fd, offset, whence);
 }
 
 int _gettimeofday(struct timeval *tv, struct timezone *tz)
