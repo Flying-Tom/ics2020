b697cd23e0e87709456caa7edf8d44e6a73ff1d3
tracer-ics2020 <tracer@njuics.org>
2020-12-04 14:50:38 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 92e439a860dc492bbd83cc78373c06e3   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  14:50:38 up  1:11,  1 user,  load average: 0.13, 0.10, 0.07

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -74,14 +74,14 @@ int _write(int fd, void *buf, size_t count)
 
 void *_sbrk(intptr_t increment)
 {
-    /*extern uint32_t end;
+    extern uint32_t end;
     uint32_t *breakpoint = &end;
     if (!_syscall_(SYS_brk, (intptr_t)(breakpoint + increment), 0, 0))
     {
         breakpoint += increment;
         return (void *)(breakpoint - increment);
     }
-    else*/
+    else
         return (void *)-1;
 }
 
