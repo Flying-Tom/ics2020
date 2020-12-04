552fa44dfd6f4d9c2c400dbf1d03621bbfe7edae
tracer-ics2020 <tracer@njuics.org>
2020-12-05 01:03:24 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 92e439a860dc492bbd83cc78373c06e3   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  01:03:24 up 11:24,  1 user,  load average: 1.37, 1.28, 1.27

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -16,18 +16,18 @@ int main()
         fscanf(fp, "%d", &n);
         assert(n == i + 1);
     }
-    printf("safe\n");
+
     fseek(fp, 0, SEEK_SET);
     for (i = 0; i < 500; i++)
     {
         fprintf(fp, "%4d\n", i + 1 + 1000);
     }
 
-    for (i = 500; i < 1500; i++)
+    for (i = 500; i < 1000; i++)
     {
         fscanf(fp, "%d", &n);
         printf("%d\n",n);
-        //assert(n == i + 1);
+        assert(n == i + 1);
     }
 
     fseek(fp, 0, SEEK_SET);
