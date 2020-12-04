0e29622cc8e04c3208267f66b63edf5d6c06a926
tracer-ics2020 <tracer@njuics.org>
2020-12-05 00:49:44 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 92e439a860dc492bbd83cc78373c06e3   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  00:49:45 up 11:10,  1 user,  load average: 1.33, 1.22, 1.25

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -34,6 +34,7 @@ void do_syscall(Context *c)
     case SYS_read:
         c->GPRx = fs_read(a[1], (void *)a[2], (size_t)a[3]);
         break;
+        /*
     case SYS_open:
         c->GPRx = fs_open((const char *)a[1], a[2], a[3]);
         break;
@@ -48,7 +49,7 @@ void do_syscall(Context *c)
         break;
     case SYS_brk:
         c->GPRx = 0;
-        break;
+        break;*/
     default:
         panic("Unhandled syscall ID = %d", a[0]);
     }
