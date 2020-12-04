7a71796ecace2fad713aa81682e0389eb5583710
tracer-ics2020 <tracer@njuics.org>
2020-12-05 01:58:14 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 92e439a860dc492bbd83cc78373c06e3   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  01:58:14 up 12:19,  1 user,  load average: 0.30, 0.32, 0.66

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -100,6 +100,8 @@ size_t fs_lseek(int fd, size_t offset, int whence)
     default:
         return -1;
     }
+    if (file_table[fd].open_offset > file_table[fd].size)
+        file_table[fd].open_offset = file_table[fd].size;
     return file_table[fd].open_offset;
 }
 int fs_close(int fd)
