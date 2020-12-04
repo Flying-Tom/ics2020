f3825586050dfe8639645f6d3bb4caead976c385
tracer-ics2020 <tracer@njuics.org>
2020-12-04 22:19:02 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 92e439a860dc492bbd83cc78373c06e3   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  22:19:02 up  8:40,  1 user,  load average: 0.08, 0.02, 0.02

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -66,6 +66,7 @@ size_t fs_read(int fd, void *buf, size_t len)
 {
     assert(fd >= 0 && fd < FILE_NUM);
     static size_t offset;
+    printf("%d\n",file_table[fd].open_offset);
     if (file_table[fd].read == NULL)
         offset = ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
     else
@@ -76,8 +77,12 @@ size_t fs_read(int fd, void *buf, size_t len)
 }
 size_t fs_write(int fd, const void *buf, size_t len)
 {
+    assert(fd >= 0 && fd < FILE_NUM);
     static size_t offset;
-    offset = ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+    if (file_table[fd].read == NULL)
+        offset = ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+    else
+        offset = ramdisk_write(buf, file_table[fd].open_offset, len);
     file_table[fd].open_offset += len;
     return offset;
 }
