c04650378018ed5d01edd85286268b61fcc68126
tracer-ics2020 <tracer@njuics.org>
2020-12-05 00:43:04 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 92e439a860dc492bbd83cc78373c06e3   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  00:43:04 up 11:04,  1 user,  load average: 1.52, 1.30, 1.27

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -30,8 +30,8 @@ void init_proc()
     Log("Initializing processes...");
 
     // load program here
-    naive_uload(NULL, "/bin/file-test");
-    //naive_uload(NULL, "/bin/hello");
+    //naive_uload(NULL, "/bin/file-test");
+    naive_uload(NULL, "/bin/hello");
 }
 
 Context *schedule(Context *prev)
