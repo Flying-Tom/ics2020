cdd83c9d63a64165c5e81e5fe2049b55b29cf431
tracer-ics2020 <tracer@njuics.org>
2020-12-04 15:28:44 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 92e439a860dc492bbd83cc78373c06e3   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  15:28:44 up  1:49,  1 user,  load average: 0.01, 0.05, 0.08

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -68,7 +68,6 @@ int _open(const char *path, int flags, mode_t mode)
 
 int _write(int fd, void *buf, size_t count)
 {
-    //_exit(SYS_write);
     return _syscall_(SYS_write, fd, (intptr_t)buf, (intptr_t)count);
 }
 
