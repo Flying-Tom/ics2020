405e85638fe8f050704156e52ae7a316ab8f750b
tracer-ics2020 <tracer@njuics.org>
2020-12-05 18:04:55 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 9f81c53aa5fc45a8b49484a29d693638   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  18:04:55 up  8:34,  1 user,  load average: 0.08, 0.15, 0.11

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -17,7 +17,7 @@ size_t serial_write(const void *buf, size_t offset, size_t len)
 {
     for (int i = 0; i < len; i++)
         putch(*(char *)(buf + i));
-    return len;
+    return 0;
 }
 
 size_t events_read(void *buf, size_t offset, size_t len)
