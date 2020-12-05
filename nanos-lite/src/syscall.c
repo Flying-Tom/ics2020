77372495229d2c797c0bb39909f3905c814f7780
tracer-ics2020 <tracer@njuics.org>
2020-12-05 18:26:33 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 9f81c53aa5fc45a8b49484a29d693638   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  18:26:33 up  8:56,  1 user,  load average: 0.12, 0.05, 0.04

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -39,7 +39,7 @@ void do_syscall(Context *c)
         c->GPRx = fs_read(a[1], (void *)a[2], a[3]);
         break;
     case SYS_write:
-        c->GPRx = sys_write(a[1], (const void *)a[2], a[3]);
+        c->GPRx = fs_write(a[1], (const void *)a[2], a[3]);
         break;
     case SYS_close:
         c->GPRx = fs_close(a[1]);
