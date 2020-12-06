2758ad1b11476797fa116e40f70d4c8c605696bb
tracer-ics2020 <tracer@njuics.org>
2020-12-06 16:22:02 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 6867b16f45174128917fa40fb9e06519   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  16:22:02 up  7:51,  0 users,  load average: 0.43, 0.18, 0.05

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -9,7 +9,7 @@ extern AM_TIMER_RTC_T rtc;
 
 int sys_gettimeofday(struct timeval *tv, struct timezone *tz)
 {
-    printf("%lld\n", io_read(AM_TIMER_UPTIME).us);
+    printf("%d\n", io_read(AM_TIMER_UPTIME).us);
     return 0;
 }
 
