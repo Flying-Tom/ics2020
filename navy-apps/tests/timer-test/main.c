14fd18d0ec8f38e1bfb97b3f26ecd5daf453024e
tracer-ics2020 <tracer@njuics.org>
2020-12-06 16:45:02 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 6867b16f45174128917fa40fb9e06519   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  16:45:02 up  8:14,  0 users,  load average: 0.23, 0.13, 0.05

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -2,21 +2,24 @@
 #include <assert.h>
 #include <sys/time.h>
 
-static uint64_t get_time()
-{
-    struct timeval now;
-    gettimeofday(&now, NULL);
-    uint32_t seconds = now.tv_sec;
-    uint32_t useconds = now.tv_usec;
-    return seconds * 1000 + (useconds + 500) / 1000;
-}
-
 int main()
 {
-    uint64_t boot_time = get_time();
+    int sec = 1;
+    struct timeval now;
     while (1)
     {
-        printf("%llu\n", get_time() - boot_time);
+        gettimeofday(&now, NULL);
+        while (now.tv_usec / 1000000 < sec)
+            ;
+        if (sec == 1)
+        {
+            printf("%d second).\n", sec);
+        }
+        else
+        {
+            printf("%d seconds).\n", sec);
+        }
+        sec++;
     }
 
     return 0;
