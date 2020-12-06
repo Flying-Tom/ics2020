63ec51037aca0001d639e56caf6efb1f57677f40
tracer-ics2020 <tracer@njuics.org>
2020-12-06 21:22:14 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 6867b16f45174128917fa40fb9e06519   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  21:22:14 up 12:52,  1 user,  load average: 0.66, 0.43, 0.21

--- abstract-machine/am/src/nemu/ioe/gpu.c
+++ abstract-machine/am/src/nemu/ioe/gpu.c
@@ -31,12 +31,12 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl)
     uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
     int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
     uint32_t *pixels = ctl->pixels;
-    int cp_bytes =  min(w, W - x);
+    int cp_bytes = min(w, W - x);
     for (int j = 0; j < h && y + j < H; j++)
     {
         //for (int i = 0; i < cp_bytes; i++)
-            //*(fb + (y + j) * W + x + i) = *(pixels + i);
-        memcpy(fb + (y + j) * W + x, pixels, 4*cp_bytes);
+        //*(fb + (y + j) * W + x + i) = *(pixels + i);
+        memcpy(fb + (y + j) * W + x, pixels, 4 * cp_bytes);
         pixels += w;
     }
 }
