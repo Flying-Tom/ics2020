64c5c2715b1d44d40130052851ef7dbe71f58981
tracer-ics2020 <tracer@njuics.org>
2020-12-06 20:49:58 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 6867b16f45174128917fa40fb9e06519   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  20:49:58 up 12:19,  1 user,  load average: 0.41, 0.27, 0.12

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -18,7 +18,8 @@ enum
     FD_STDIN,
     FD_STDOUT,
     FD_STDERR,
-    FD_FB
+    FD_FB,
+    FD_EVENT
 };
 
 size_t invalid_read(void *buf, size_t offset, size_t len)
@@ -34,19 +35,20 @@ size_t invalid_write(const void *buf, size_t offset, size_t len)
 }
 
 extern size_t serial_write(const void *buf, size_t offset, size_t len);
+extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
+extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+extern size_t events_read(void *buf, size_t offset, size_t len);
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
     [FD_STDIN] = {"stdin", 0, 0, 0, invalid_read, invalid_write},
     [FD_STDOUT] = {"stdout", 0, 0, 0, invalid_read, serial_write},
     [FD_STDERR] = {"stderr", 0, 0, 0, invalid_read, serial_write},
+    [FD_EVENT] = {"/dev/events", 0, 0, 0, events_read, invalid_write},
 #include "files.h"
 };
 
 #define FILE_NUM sizeof(file_table) / sizeof(Finfo)
-
-size_t ramdisk_read(void *buf, size_t offset, size_t len);
-size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 
 void init_fs()
 {
