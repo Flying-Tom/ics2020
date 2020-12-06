631a77fa7be99c85378255727383fe4c24534e61
tracer-ics2020 <tracer@njuics.org>
2020-12-06 10:45:13 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 6867b16f45174128917fa40fb9e06519   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  10:45:13 up  2:14,  0 users,  load average: 0.06, 0.03, 0.00

---
+++ navy-apps/tests/timer-test/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <assert.h>
+#include <sys/time.h>
+
+static uint64_t get_time()
+{
+    struct timeval now;
+    gettimeofday(&now, NULL);
+    uint32_t seconds = now.tv_sec;
+    uint32_t useconds = now.tv_usec;
+    return seconds * 1000 + (useconds + 500) / 1000;
+}
+
+int main()
+{
+    get_time();
+    return 0;
+}
