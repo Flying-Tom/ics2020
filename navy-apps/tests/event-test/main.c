9566ffbbd7490e93af7692dac99585a23abc4195
tracer-ics2020 <tracer@njuics.org>
2020-12-06 23:39:50 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 6867b16f45174128917fa40fb9e06519   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  23:39:50 up 15:09,  1 user,  load average: 0.81, 0.43, 0.22

--- navy-apps/tests/event-test/main.c
+++ navy-apps/tests/event-test/main.c
@@ -8,11 +8,11 @@ int main()
     while (1)
     {
         char buf[64];
+        printf("%d\n", cnt++);
         if (NDL_PollEvent(buf, sizeof(buf)))
         {
             printf("receive event: %s\n", buf);
         }
-        printf("%d\n", cnt++);
     }
     return 0;
 }
