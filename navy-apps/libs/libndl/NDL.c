298a24e501c946f535cd3858bc2d9eb2fda06e1b
tracer-ics2020 <tracer@njuics.org>
2020-12-07 14:32:44 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: fc4db119baa948618582b7084b4b0f75   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  14:32:44 up  5:02,  1 user,  load average: 0.15, 0.21, 0.15

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -18,7 +18,6 @@ uint32_t NDL_GetTicks()
 int NDL_PollEvent(char *buf, int len)
 {
     FILE *fp = fopen("/dev/events", "r");
-    buf = "\0";
     int ret = fread(buf, 1, sizeof(buf), fp);
     //printf("%s\n",buf);
     printf("%d\n",strlen(buf));
