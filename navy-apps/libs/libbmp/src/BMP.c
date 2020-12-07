03c04d71d54b4f725583d62f59979e71160d6642
tracer-ics2020 <tracer@njuics.org>
2020-12-08 00:10:01 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: fc4db119baa948618582b7084b4b0f75   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  00:10:01 up 14:39,  1 user,  load average: 0.18, 0.09, 0.06

--- navy-apps/libs/libbmp/src/BMP.c
+++ navy-apps/libs/libbmp/src/BMP.c
@@ -27,9 +27,9 @@ void *BMP_Load(const char *filename, int *width, int *height)
         return NULL;
     
     struct BitmapHeader hdr;
-    assert(sizeof(hdr) == 54);printf("Checkpoint\n");
+    assert(sizeof(hdr) == 54);
     assert(1 == fread(&hdr, sizeof(struct BitmapHeader), 1, fp));
-
+    printf("Checkpoint\n");
     if (hdr.bitcount != 24)
         return NULL;
     if (hdr.compression != 0)
