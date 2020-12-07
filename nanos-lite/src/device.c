bfa93b7c80018780a8c76057d05529c06dec55a4
tracer-ics2020 <tracer@njuics.org>
2020-12-07 15:25:51 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: fc4db119baa948618582b7084b4b0f75   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  15:25:51 up  5:55,  1 user,  load average: 0.22, 0.14, 0.10

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -23,9 +23,8 @@ size_t serial_write(const void *buf, size_t offset, size_t len)
 
 size_t events_read(void *buf, size_t offset, size_t len)
 {
-    //bool keydown = io_read(AM_INPUT_KEYBRD).keydown;
-    int keycode = io_read(AM_INPUT_KEYBRD).keycode;
     bool keydown = io_read(AM_INPUT_KEYBRD).keydown;
+    int keycode = io_read(AM_INPUT_KEYBRD).keycode;
     if (keycode != AM_KEY_NONE)
     {
         if (keydown)
