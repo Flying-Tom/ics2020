78de0fa4a9230617e9a6c4498df1ef27b233f0db
tracer-ics2020 <tracer@njuics.org>
2020-12-07 23:25:25 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: fc4db119baa948618582b7084b4b0f75   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  23:25:25 up 13:55,  1 user,  load average: 0.22, 0.20, 0.13

--- nanos-lite/include/fs.h
+++ nanos-lite/include/fs.h
@@ -18,4 +18,10 @@ size_t fs_write(int fd, const void *buf, size_t len);
 size_t fs_lseek(int fd, size_t offset, int whence);
 int fs_close(int fd);
 
+size_t serial_write(const void *buf, size_t offset, size_t len);
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+size_t events_read(void *buf, size_t offset, size_t len);
+size_t fb_write(const void *buf, size_t offset, size_t len);
+
 #endif
