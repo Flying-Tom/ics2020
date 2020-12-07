e9694e9e28c014bb1d804714fd2abe9803198d9a
tracer-ics2020 <tracer@njuics.org>
2020-12-07 15:30:47 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: fc4db119baa948618582b7084b4b0f75   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  15:30:47 up  6:00,  1 user,  load average: 0.08, 0.14, 0.10

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -30,14 +30,14 @@ size_t events_read(void *buf, size_t offset, size_t len)
     {
         if (keydown)
         {
-            sprintf(buf, "kd %s\n", keyname[keycode]);
+            //sprintf(buf, "kd %s\n", keyname[keycode]);
             printf("kd %s\n", keyname[keycode]);
         }
-        /*else
+        else
         {
-            sprintf(buf, "ku %s\n", keyname[keycode]);
+            //sprintf(buf, "ku %s\n", keyname[keycode]);
             printf("ku %s\n", keyname[keycode]);
-        }*/
+        }
     }
     return strlen(buf);
 }
