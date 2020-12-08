0c43d3a9af0abdb79f84dd75455cdb468773c859
tracer-ics2020 <tracer@njuics.org>
2020-12-08 11:24:48 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: e07387fcace640d9abe536f3a62a50dd   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  11:24:48 up  1:54,  0 users,  load average: 0.09, 0.15, 0.07

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -8,8 +8,8 @@ int main()
 {
     NDL_Init(0);
     int w, h;
-    uint32_t *test = malloc(128 * 128 * sizeof(uint32_t));
-    assert(test);
+    //uint32_t *test = malloc(128 * 128 * sizeof(uint32_t));
+    //assert(test);
     void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
     assert(bmp);
     NDL_OpenCanvas(&w, &h);
