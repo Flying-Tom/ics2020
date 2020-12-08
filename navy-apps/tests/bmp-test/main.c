df69cbf04af92a4fc8aff862eab926ab0b9f1c53
tracer-ics2020 <tracer@njuics.org>
2020-12-08 17:20:38 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: e07387fcace640d9abe536f3a62a50dd   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  17:20:38 up  7:50,  1 user,  load average: 0.07, 0.05, 0.06

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -4,6 +4,15 @@
 #include <NDL.h>
 #include <BMP.h>
 
+void test()
+{
+    FILE *fp = fopen("/proc/dispinfo", "r");
+    char buf[128];
+    fread(buf,1,128,fp);
+    printf("%s\n",buf);
+    fclose(fp);
+}
+
 int main()
 {
     NDL_Init(0);
@@ -11,7 +20,7 @@ int main()
     void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
     assert(bmp);
     NDL_OpenCanvas(&w, &h);
-    printf("w:%d h:%d\n", w, h);
+    test(); /////////
     NDL_DrawRect(bmp, 0, 0, w, h);
     free(bmp);
     NDL_Quit();
