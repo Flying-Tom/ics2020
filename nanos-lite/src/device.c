5ec0f4000005a165ed0ea579b506282c52602898
tracer-ics2020 <tracer@njuics.org>
2020-12-08 17:21:32 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: e07387fcace640d9abe536f3a62a50dd   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  17:21:32 up  7:51,  1 user,  load average: 0.17, 0.09, 0.08

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -39,12 +39,10 @@ size_t events_read(void *buf, size_t offset, size_t len)
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len)
 {
-    assert(false);
-    /*
     static char dispinfo[32] = {};
 
     sprintf(dispinfo, "WIDTH:%d\nHEIGHT:%d\n", io_read(AM_GPU_CONFIG).width, io_read(AM_GPU_CONFIG).height);
-    printf("WIDTH:%d\nHEIGHT:%d\n", io_read(AM_GPU_CONFIG).width, io_read(AM_GPU_CONFIG).height);*/
+    printf("WIDTH:%d\nHEIGHT:%d\n", io_read(AM_GPU_CONFIG).width, io_read(AM_GPU_CONFIG).height);
     return len;
 }
 
