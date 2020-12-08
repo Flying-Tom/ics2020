69c2c403d8357d7c22864988e229f389b5fdc114
tracer-ics2020 <tracer@njuics.org>
2020-12-08 10:54:31 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: e07387fcace640d9abe536f3a62a50dd   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  10:54:31 up  1:24,  0 users,  load average: 0.25, 0.11, 0.03

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -40,7 +40,7 @@ size_t events_read(void *buf, size_t offset, size_t len)
 size_t dispinfo_read(void *buf, size_t offset, size_t len)
 {
     assert(offset == 0);
-    assert(0);
+    assert(false);
     int fp = fs_open("/proc/dispinfo", 0, 0);
     fs_read(fp, buf + offset, len);
     fs_close(fp);
