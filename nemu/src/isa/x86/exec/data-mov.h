e42ea1a698682bafefb9a40f4c335fc6fdb4aaf2
tracer-ics2020 <tracer@njuics.org>
2020-12-09 09:01:55 +0800

    >  compile 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 7f3a74a90fac478bbc19ce2288a61e0a   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  09:01:55 up  1:20,  0 users,  load average: 0.02, 0.06, 0.02

--- nemu/src/isa/x86/exec/data-mov.h
+++ nemu/src/isa/x86/exec/data-mov.h
@@ -121,7 +121,7 @@ static inline def_EHelper(movsb)
     rtl_lr(s, s0, R_EDI, 4);
     rtl_addi(s, s0, s0, id_dest->width);
     rtl_sr(s, R_EDI, s0, 4);
-    print_asm_template1(movsb);
+    print_asm(str(movsb) "%c %%esi,%%edi", suffix_char(id_dest->width), id_src1->str, id_dest->str);
 }
 
 static inline def_EHelper(lea)
