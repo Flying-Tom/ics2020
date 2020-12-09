756c1c6ecf6379e00e90b44c6c5aa00d79057c2d
tracer-ics2020 <tracer@njuics.org>
2020-12-10 00:24:40 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: ef339ab024df44f7855865d557cb13e8   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  00:24:40 up 11:07,  1 user,  load average: 0.52, 0.67, 0.68

--- navy-apps/apps/nslider/src/main.cpp
+++ navy-apps/apps/nslider/src/main.cpp
@@ -67,7 +67,6 @@ int main()
         if (e.type == SDL_KEYDOWN)
         {
             printf("%d\n",e.key.keysym.sym);
-            render();
             switch (e.key.keysym.sym)
             {
             case SDLK_0:
