8fa3edfcb70c583372163cbe0202f644092c9dfd
tracer-ics2020 <tracer@njuics.org>
2020-12-10 20:45:37 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: d0cae43321ee4f35b69edf05cfa8aef0   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  20:45:37 up 11:15,  1 user,  load average: 0.17, 0.17, 0.22

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -48,7 +48,7 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color)
         for (int j = 0; j < dstrect->h; j++)
             for (int i = 0; i < dstrect->w; i++)
             {
-                printf("j:%d i:%d\n", j, i);
+                //printf("j:%d i:%d\n", j, i);
                 int loc = ((j + dstrect->y) >= dst->h ? (dst->h - 1) : (j + dstrect->y)) * dst->w + ((i + dstrect->x) >= dst->w ? (dst->w - 1) : (i + dstrect->x));
                 pixels_tmp[loc] = color;
             }
