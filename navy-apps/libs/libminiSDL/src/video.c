f9b9d5a4b3bca8fd575dc904b773b5bd8b8856be
tracer-ics2020 <tracer@njuics.org>
2020-12-12 03:22:43 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: f687f7c292ef44bc98e3bf3885ce82ef   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  03:22:43 up 17:52,  1 user,  load average: 0.13, 0.15, 0.10

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -40,7 +40,10 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
             int dst_loc = 0, src_loc = 0;
             printf("dst_loc:%d\n", dst_loc);
             if (j + dstrect->y >= dst->h)
+            {   
                 dst_loc += (dst->h - 1) * dst->w;
+                printf("dst_loc:%d\n", dst_loc);
+            }
             else
                 dst_loc += (j + dstrect->y) * dst->w;
             printf("dst_loc:%d\n", dst_loc);
@@ -48,7 +51,6 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
                 dst_loc += dst->w - 1;
             else
                 dst_loc += i + dstrect->x;
-            printf("dst_loc:%d\n", dst_loc);
 
             if (j + srcrect->y >= src->h)
                 src_loc += (src->h - 1) * src->w;
@@ -59,7 +61,6 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
             else
                 src_loc += i + srcrect->x;
 
-            printf("src_loc:%d\n", src_loc);
             if (src->format->palette == NULL)
                 dst_pixels_buf[dst_loc] = src_pixels_buf[src_loc];
             else
