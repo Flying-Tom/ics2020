b67ec885431598127b43100bb8fc5ca523db7e13
tracer-ics2020 <tracer@njuics.org>
2020-12-12 03:27:42 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: f687f7c292ef44bc98e3bf3885ce82ef   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  03:27:42 up 17:57,  1 user,  load average: 0.61, 0.25, 0.14

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -63,8 +63,8 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
             else
                 src_loc += i + srcrect->x;
 
-            //if (src->format->palette == NULL)
-            //    dst_pixels_buf[dst_loc] = src_pixels_buf[src_loc];
+            if (src->format->palette == NULL)
+                dst_pixels_buf[dst_loc] = src_pixels_buf[src_loc];
             //else
                 //((uint8_t *)dst_pixels_buf)[dst_loc] = ((uint8_t *)src_pixels_buf)[src_loc];
         }
