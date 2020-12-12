434871757f9349a2597c24feb68d6fdec35a3613
tracer-ics2020 <tracer@njuics.org>
2020-12-12 13:01:05 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: cb71afa09c29466c9e9cf6fc1ec67f4a   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  13:01:05 up  3:30,  1 user,  load average: 0.17, 0.18, 0.14

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -83,7 +83,7 @@ int main(int argc, char *argv[], char *envp[])
     logo_sf = SDL_LoadBMP("/share/pictures/projectn.bmp");
     assert(logo_sf);
     set_i_max();
-
+    assert(0);
     while (1)
     {
         display_menu(i_max);
@@ -158,7 +158,7 @@ int main(int argc, char *argv[], char *envp[])
 
 static void draw_ch(BDF_Font *font, int x, int y, char ch, uint32_t fg, uint32_t bg)
 {
-    printf("x:%d y:%d\n", x, y);
+    //printf("x:%d y:%d\n", x, y);
     SDL_Surface *s = BDF_CreateSurface(font, ch, fg, bg);
     SDL_Rect dstrect = {.x = x, .y = y};
     SDL_BlitSurface(s, NULL, screen, &dstrect);
