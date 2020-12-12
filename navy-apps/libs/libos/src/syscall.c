ab0a0815443b54572b63c3587f2d8428e2ee01b0
tracer-ics2020 <tracer@njuics.org>
2020-12-13 02:21:46 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: cb71afa09c29466c9e9cf6fc1ec67f4a   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  02:21:46 up 16:51,  1 user,  load average: 0.00, 0.07, 0.07

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -86,7 +86,7 @@ void *_sbrk(intptr_t increment)
 
 int _read(int fd, void *buf, size_t count)
 {
-    return _syscall_(SYS_read, fd, (intptr_t)buf, (intptr_t)count);
+    return _syscall_(SYS_read, fd, (intptr_t)buf, count);
 }
 
 int _close(int fd)
