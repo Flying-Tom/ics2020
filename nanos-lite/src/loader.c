e9d50e0c3a5811b188c2dbde3a5e2724cb239c34
tracer-ics2020 <tracer@njuics.org>
2020-12-13 02:11:28 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: cb71afa09c29466c9e9cf6fc1ec67f4a   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  02:11:28 up 16:41,  1 user,  load average: 0.19, 0.13, 0.06

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -14,22 +14,24 @@ static uintptr_t loader(PCB *pcb, const char *filename)
 {
 
     Elf_Ehdr elf;
-    Log("Loader:%s", filename);
+    //Log("Loader:%s", filename);
     int fd = fs_open(filename, 0, 0);
     fs_read(fd, (void *)&elf, sizeof(Elf_Ehdr));
-    Elf_Phdr phdr[elf.e_phnum];
+    //Elf_Phdr phdr[elf.e_phnum];
 
     for (size_t i = 0; i < elf.e_phnum; i++)
     {
+        Elf_Phdr phdr;
         fs_lseek(fd, elf.e_phoff + i * elf.e_phentsize, SEEK_SET);
-        fs_read(fd, (void *)&phdr[i], elf.e_phentsize);
-        if (phdr[i].p_type == PT_LOAD)
+        fs_read(fd, (void *)&phdr, elf.e_phentsize);
+        if (phdr.p_type == PT_LOAD)
         {
-            fs_lseek(fd, phdr[i].p_offset, SEEK_SET);
-            fs_read(fd, (void *)phdr[i].p_vaddr, phdr[i].p_filesz);
-            memset((void *)(phdr[i].p_vaddr + phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz);
+            fs_lseek(fd, phdr.p_offset, SEEK_SET);
+            fs_read(fd, (void *)phdr.p_vaddr, phdr.p_filesz);
+            memset((void *)(phdr.p_vaddr + phdr.p_filesz), 0, phdr.p_memsz - phdr.p_filesz);
         }
     }
+    fs_close(fd);
     return elf.e_entry;
 }
 
--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -30,8 +30,8 @@ void init_proc()
     Log("Initializing processes...");
 
     // load program here
-    naive_uload(NULL, "/bin/bird");
-    //naive_uload(NULL, "/bin/menu");
+    //naive_uload(NULL, "/bin/bird");
+    naive_uload(NULL, "/bin/menu");
     //naive_uload(NULL, "/bin/nslider");
     //naive_uload(NULL, "/bin/nterm");
     //naive_uload(NULL, "/bin/bmp-test");
