9b4ec0c7c21435e52eb2c02f768147b9f3f65bbf
tracer-ics2020 <tracer@njuics.org>
2020-12-12 17:54:28 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: cb71afa09c29466c9e9cf6fc1ec67f4a   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  17:54:29 up  8:24,  1 user,  load average: 0.26, 0.15, 0.05

--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -1,27 +1,41 @@
-#define SDL_malloc  malloc
-#define SDL_free    free
+#define SDL_malloc malloc
+#define SDL_free free
 #define SDL_realloc realloc
 
 #define SDL_STBIMAGE_IMPLEMENTATION
 #include "SDL_stbimage.h"
 
-SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
-  assert(src->type == RW_TYPE_MEM);
-  return NULL;
+SDL_Surface *IMG_Load_RW(SDL_RWops *src, int freesrc)
+{
+    assert(src->type == RW_TYPE_MEM);
+    return NULL;
 }
 
-SDL_Surface* IMG_Load(const char *filename) {
-  return NULL;
+SDL_Surface *IMG_Load(const char *filename)
+{
+    FILE *fp = fopen(filename, "r");
+    int size = -1;
+    fseek(fp, 0, SEEK_END);
+    size = ftell(fp);
+    assert(size > 0);
+    char *buf = SDL_malloc();
+    SDL_Surface *sur_temp = STBIMG_LoadFromMemory(buf, size);
+    fclose(fp);
+    SDL_free(buf);
+    return sur_temp;
 }
 
-int IMG_isPNG(SDL_RWops *src) {
-  return 0;
+int IMG_isPNG(SDL_RWops *src)
+{
+    return 0;
 }
 
-SDL_Surface* IMG_LoadJPG_RW(SDL_RWops *src) {
-  return IMG_Load_RW(src, 0);
+SDL_Surface *IMG_LoadJPG_RW(SDL_RWops *src)
+{
+    return IMG_Load_RW(src, 0);
 }
 
-char *IMG_GetError() {
-  return "Navy does not support IMG_GetError()";
+char *IMG_GetError()
+{
+    return "Navy does not support IMG_GetError()";
 }
