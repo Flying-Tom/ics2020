8722bf64c447b457ede66ec7290ca72533f2712b
tracer-ics2020 <tracer@njuics.org>
2020-12-12 13:09:47 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: cb71afa09c29466c9e9cf6fc1ec67f4a   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  13:09:48 up  3:39,  1 user,  load average: 0.20, 0.15, 0.11

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -78,6 +78,7 @@ int main(int argc, char *argv[], char *envp[])
 {
     SDL_Init(0);
     screen = SDL_SetVideoMode(0, 0, 32, SDL_HWSURFACE);
+    printf("screen_w:%d screen_h:%d\n", screen->w, screen->h);
 
     font = new BDF_Font(font_fname);
     logo_sf = SDL_LoadBMP("/share/pictures/projectn.bmp");
