5dbbb30063e28305c568621322ad0ba291b2968b
tracer-ics2020 <tracer@njuics.org>
2020-12-13 12:59:14 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: e2c8bc96a61c4599b72604ad73e7e269   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  12:59:14 up  3:29,  1 user,  load average: 0.33, 0.24, 0.21

--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -13,7 +13,7 @@ SDL_Surface *IMG_Load_RW(SDL_RWops *src, int freesrc)
 
 SDL_Surface *IMG_Load(const char *filename)
 {
-    /*
+    
     FILE *fp = fopen(filename, "r");
     fseek(fp, 0, SEEK_END);
     int size = ftell(fp);
@@ -24,8 +24,9 @@ SDL_Surface *IMG_Load(const char *filename)
     SDL_Surface *sur_temp = STBIMG_LoadFromMemory(buf, size);
     SDL_free(buf);
     fclose(fp);
-    return sur_temp;*/
-    return NULL;
+    assert(0);
+    return sur_temp;
+
 }
 
 int IMG_isPNG(SDL_RWops *src)
