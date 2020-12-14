c9dcf5e4b28d31de97f72dfcbcfd518bfdbe48e9
tracer-ics2020 <tracer@njuics.org>
2020-12-14 22:40:56 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 5a475e244c7243e0a14fc2fbb5af714b   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  22:40:56 up 13:10,  1 user,  load average: 0.24, 0.23, 0.16

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -102,11 +102,11 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h)
         assert(pixels_buf);
         memset(pixels_buf, 0, w * h * sizeof(uint32_t));
         int cnt = 0;
-        for (int i = 0; i < h ; i++)
+        for (int i = 0; i < h && y + i < s->h; i++)
         {
-            for (int j = 0; j < w ; j++)
+            for (int j = 0; j < w && x + j < s->w; j++)
             {
-                pixels_buf[cnt++] = s->format->palette->colors[s->pixels[(y + j) * s->w + x + i]].val;
+                pixels_buf[cnt++] = s->format->palette->colors[s->pixels[(y + j) * s->w + x + i]].val+0xff;
             }
         }
         NDL_DrawRect(pixels_buf, x, y, w, h);
