bcd7c6ff88579cacebc6eec5374c35c4526bffa0
tracer-ics2020 <tracer@njuics.org>
2020-12-14 21:30:56 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 5a475e244c7243e0a14fc2fbb5af714b   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  21:30:56 up 12:00,  1 user,  load average: 0.51, 0.24, 0.13

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -4,6 +4,14 @@
 #include <string.h>
 #include <stdlib.h>
 
+static int min(int x, int y)
+{
+    if (x < y)
+        return x;
+    else
+        return y;
+}
+
 void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_Rect *dstrect)
 {
     assert(dst && src);
