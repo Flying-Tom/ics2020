ccc35624971438383cb3d82ba36a69719e9932d4
tracer-ics2020 <tracer@njuics.org>
2020-12-16 00:59:54 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: a1106d9709a7487491a2453352b475a4   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  00:59:54 up 15:29,  1 user,  load average: 0.73, 0.37, 0.30

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -62,7 +62,7 @@ uint8_t *SDL_GetKeyState(int *numkeys)
     static uint8_t KeyState[sizeof(keyname) / sizeof(keyname[0])] = {};
 
     static SDL_Event ev;
-    while (SDL_PollEvent(&ev))
+    while (!SDL_PollEvent(&ev))
     { //printf("Enter\n");
         KeyState[ev.key.keysym.sym] = 1;
         printf("%s\n", keyname[ev.key.keysym.sym]);
