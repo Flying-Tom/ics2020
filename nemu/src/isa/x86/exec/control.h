700c73d0f8cfd93a97d10890dac3b98618ced867
tracer-ics2020 <tracer@njuics.org>
2020-12-16 16:21:07 +0800

    >  compile 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 24d1ab3469e74b99a674e26998de906c   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  16:21:07 up  4:30,  1 user,  load average: 0.59, 0.46, 0.38

--- nemu/src/isa/x86/exec/control.h
+++ nemu/src/isa/x86/exec/control.h
@@ -73,8 +73,8 @@ static inline def_EHelper(loopne)
     rtl_sub(s, &cpu.ecx, &cpu.ecx, s0);
 
     printf("cpu.ecx:%x\n", cpu.ecx);
-    
-    *s0 = (cpu.eflags.ZF == 0) && (cpu.ecx > 0);
+
+    *s0 = (cpu.eflags.ZF == 0) && (cpu.ecx != 0);
     rtl_sext(s, s1, dsrc1, 4);
 
     printf("dsrc1:%x\n", *dsrc1);
@@ -82,7 +82,7 @@ static inline def_EHelper(loopne)
     printf("address:%x\n", s->seq_pc + *s1);
     if (*s0)
     {
-        rtl_j(s, s->seq_pc + *s1);
+        //rtl_j(s, s->seq_pc + *s1);
     }
 
     print_asm("loopne *%s", id_dest->str);
