65fdc7d63b971e9dd1870b96a0ba00a7b730e5ad
tracer-ics2020 <tracer@njuics.org>
2020-12-16 15:37:46 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 24d1ab3469e74b99a674e26998de906c   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  15:37:46 up  3:47,  1 user,  load average: 0.51, 0.49, 0.82

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -39,8 +39,8 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
     for (int j = 0; j < srcrect->h; j++)
         for (int i = 0; i < srcrect->w; i++)
         {
-            int src_loc = min(j + srcrect->y, srcrect->h - 1) * srcrect->w + min(i + srcrect->x, srcrect->w - 1);
-            int dst_loc = min(j + dstrect->y, dstrect->h - 1) * dstrect->w + min(i + dstrect->x, dstrect->w - 1);
+            int src_loc = min(j + srcrect->y, srcrect->h - 1) * src->w + min(i + srcrect->x, src->w - 1);
+            int dst_loc = min(j + dstrect->y, dstrect->h - 1) * dst->w + min(i + dstrect->x, dst->w - 1);
             if (src->format->palette == NULL)
                 ((uint32_t *)dst->pixels)[dst_loc] = ((uint32_t *)src->pixels)[src_loc];
             else
