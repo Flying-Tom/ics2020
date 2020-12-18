8e4435a4c0b824ae618fee1ba37759124000a3a1
tracer-ics2020 <tracer@njuics.org>
2020-12-18 18:12:37 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 5370b758e4e849bf9d799c2883304a63   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  18:12:37 up  8:42,  1 user,  load average: 0.31, 0.37, 0.39

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -18,7 +18,7 @@ uint32_t NDL_GetTicks()
     uint32_t usec = (now.tv_usec - boot_time.tv_usec) % 1000000;
     uint32_t sec = (now.tv_usec - boot_time.tv_usec) / 1000000;
     uint32_t msec = sec * 1000 + usec / 1000;
-    printf("now.tv_sec:%u\n", now.tv_sec);
+    //printf("now.tv_sec:%u\n", now.tv_sec);
     printf("now.tv_usec:%u\n", now.tv_usec);
     return msec;
 }
