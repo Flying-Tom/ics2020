3b8c33a80d5f1fe58fd0227e2d90ce6cd551f379
tracer-ics2020 <tracer@njuics.org>
2021-01-21 15:37:20 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: f1d1864ece644c68b933bb61898cc61f   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-38-generic       Architecture: x86-64  15:37:20 up  6:50,  1 user,  load average: 0.84, 0.60, 0.61

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -31,14 +31,14 @@ void init_proc()
 
     // load program here
     //naive_uload(NULL, "/bin/bird");
-    naive_uload(NULL, "/bin/pal");
+    //naive_uload(NULL, "/bin/pal");
     //naive_uload(NULL, "/bin/menu");
     //naive_uload(NULL, "/bin/menu");
     //naive_uload(NULL, "/bin/nslider");
     //naive_uload(NULL, "/bin/nterm");
     //naive_uload(NULL, "/bin/bmp-test");
     //naive_uload(NULL, "/bin/event-test");
-    //naive_uload(NULL, "/bin/timer-test");
+    naive_uload(NULL, "/bin/timer-test");
     //naive_uload(NULL, "/bin/file-test");
     //naive_uload(NULL, "/bin/dummy");
 }
