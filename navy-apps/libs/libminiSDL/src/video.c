8fc53a8bce5386fb801536a8bcfc0f60744e9e96
tracer-ics2020 <tracer@njuics.org>
2021-01-22 03:41:17 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: f1d1864ece644c68b933bb61898cc61f   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-38-generic       Architecture: x86-64  03:41:17 up 18:54,  1 user,  load average: 0.65, 0.75, 0.52

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -35,11 +35,11 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
         assert(dstrect);
         dstrect->x = 0;
         dstrect->y = 0;
-        dstrect->w = srcrect->w;
-        dstrect->h = srcrect->h;
     }
+    dstrect->w = srcrect->w;
+    dstrect->h = srcrect->h;
 
-    //assert(srcrect->h == dstrect->h && srcrect->w == dstrect->w);
+    assert(srcrect->h == dstrect->h && srcrect->w == dstrect->w);
     for (int j = 0; j < srcrect->h; j++)
         for (int i = 0; i < srcrect->w; i++)
         {
