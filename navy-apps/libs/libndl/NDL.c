fce2ae9a7b6d6c45d38407ccdfbe246845445435
tracer-ics2020 <tracer@njuics.org>
2021-01-21 21:49:21 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: f1d1864ece644c68b933bb61898cc61f   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-38-generic       Architecture: x86-64  21:49:21 up 13:02,  1 user,  load average: 1.07, 0.36, 0.15

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -85,17 +85,14 @@ void NDL_OpenCanvas(int *w, int *h)
 
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h)
 {
-    /*
     for (int j = 0; j < h; j++)
         for (int i = 0; i < w; i++)
             canvas[(j + y) * canvas_w + (i + x)] = pixels[j * w + i];
-    */
-    int wtemp = w <= screen_w - x ? w : screen_w - x;
-    int htemp = h <= screen_h - y ? h : screen_h - y;
-    for (int i = 0; i < htemp; i++)
+
+    for (int i = 0; i < canvas_h; i++)
     {
-        lseek(fbdev, (i + y) * screen_w + x, SEEK_SET);
-        write(fbdev, &pixels[i * w], wtemp);
+        lseek(fbdev, ((i + space_h) * screen_w + space_w) * sizeof(uint32_t), SEEK_SET);
+        write(fbdev, &canvas[i * canvas_w], canvas_w);
     }
 }
 
