18936913548bc21a62ff209ffc8f9762c2131c84
tracer-ics2020 <tracer@njuics.org>
2021-01-22 15:17:37 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 33a3f1ca6f5a44d9bd2813effd6ce791   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-38-generic       Architecture: x86-64  15:17:37 up  5:47,  1 user,  load average: 0.43, 0.99, 1.46

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -18,7 +18,7 @@ static int min(int x, int y)
 void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_Rect *dstrect)
 {
     assert(dst && src);
-    printf("Enter Blit\n");
+    //printf("Enter Blit\n");
     SDL_Rect tmprect;
     if (srcrect == NULL)
     {
