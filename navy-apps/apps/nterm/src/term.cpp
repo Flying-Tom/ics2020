1f09a39f527e9c7fade3cd1288a3baecd06a2f1d
tracer-ics2020 <tracer@njuics.org>
2021-01-23 02:02:16 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 33a3f1ca6f5a44d9bd2813effd6ce791   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-38-generic       Architecture: x86-64  02:02:16 up 16:32,  1 user,  load average: 0.09, 0.47, 0.66

--- navy-apps/apps/nterm/src/term.cpp
+++ navy-apps/apps/nterm/src/term.cpp
@@ -47,23 +47,6 @@ Terminal::Pattern Terminal::esc_seqs[] = {
     {"\033[#;#;#m", &Terminal::esc_setattr3},
 };
 
-struct BinItem
-{
-    const char *name, *bin, *arg1;
-} items[] = {
-    {"NJU Terminal", "/bin/nterm", NULL},
-    {"NSlider", "/bin/nslider", NULL},
-    {"FCEUX (Super Mario Bros)", "/bin/fceux", "/share/games/nes/mario.nes"},
-    {"FCEUX (100 in 1)", "/bin/fceux", "/share/games/nes/100in1.nes"},
-    {"Flappy Bird", "/bin/bird", NULL},
-    {"PAL - Xian Jian Qi Xia Zhuan", "/bin/pal", NULL},
-    {"NPlayer", "/bin/nplayer", NULL},
-    {"coremark", "/bin/coremark", NULL},
-    {"dhrystone", "/bin/dhrystone", NULL},
-    {"typing-game", "/bin/typing-game", NULL},
-    {"ONScripter", "/bin/onscripter", NULL},
-};
-
 static inline int min(int x, int y) { return x < y ? x : y; }
 static inline int max(int x, int y) { return x > y ? x : y; }
 
@@ -239,33 +222,6 @@ Terminal::~Terminal()
     delete[] dirty;
 }
 
-//////////////////////
-
-void input_process(char *input)
-{
-    /*
-    for (int i = 0; i < sizeof(items) / sizeof(BinItem); i++)
-    {
-        if (strcmp(items[i].name, input) == 0)
-        {
-            const char *exec_argv[3];
-            auto *item = &items[i];
-            exec_argv[0] = item->bin;
-            exec_argv[1] = item->arg1;
-            exec_argv[2] = NULL;
-            execve(exec_argv[0], (char **)exec_argv, NULL);
-            return;
-        }
-    }
-    execve(input, NULL, NULL);
-    printf("%s: command not found\n");
-    */
-    setenv("PATH", "/bin", 0);
-    execvp(input, NULL);
-}
-
-//////////////////////
-
 void Terminal::backspace()
 {
     cursor.x--;
@@ -413,8 +369,8 @@ const char *Terminal::keypress(char ch)
             break;
         case '\n':
             strcpy(cooked, input);
-            //printf("%s\n", input);
-            input_process(input);
+            setenv("PATH", "/bin", 0);
+            execvp(input, NULL);
             strcat(cooked, "\n");
             ret = cooked;
             write("\n", 1);
