53aaff20ef5ba7f3a4f1ae522aec9eba4b22bd98
tracer-ics2020 <tracer@njuics.org>
2021-01-22 13:14:23 +0800

    >  run 191250004    Static hostname: ubuntu          Icon name: computer-desktop            Chassis: desktop         Machine ID: 690c72cb13c14d9c94df839eed7e3ded            Boot ID: 33a3f1ca6f5a44d9bd2813effd6ce791   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-38-generic       Architecture: x86-64  13:14:23 up  3:44,  1 user,  load average: 0.23, 0.11, 0.04

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -17,7 +17,7 @@ static int min(int x, int y)
 
 void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_Rect *dstrect)
 {
-    assert(dst && src && dst->format->BitsPerPixel == src->format->BitsPerPixel);
+    assert(dst && src);
     printf("Enter Blit\n");
     if (srcrect == NULL)
     {
@@ -53,6 +53,7 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
 
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color)
 {
+    printf("Enter Update\n");
     SDL_Rect rect_tmp;
     if (dstrect == NULL)
     {
